f17ed07bd578cea5e3147a31a235dcf5c3b1aa4f
Ludovico Magnocavallo <ludomagno@google.com>
2019-09-07 14:09:48 +0200

    sample test

---
+++ tests/organization-bootstrap/environments/test_plan.py
@@ -0,0 +1,37 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"Test plan on the organization environment sample."
+
+import os
+import re
+
+import pytest
+import tftest
+
+
+_ABSPATH = os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)
+_TFDIR = os.path.sep.join(_ABSPATH[-2:])
+
+
+@pytest.fixture(scope='module')
+def plan():
+  tf = tftest.TerraformTest(_TFDIR, os.path.sep.join(_ABSPATH[:-3]),
+                            os.environ.get('TERRAFORM', 'terraform'))
+  tf.setup(extra_files=['tests/{}/terraform.tfvars'.format(_TFDIR)])
+  return tf.plan()
+
+
+def test_dummy(plan):
+  print(plan)
