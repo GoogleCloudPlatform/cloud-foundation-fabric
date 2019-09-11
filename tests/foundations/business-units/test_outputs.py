f17b4a9b0415bb61368804d8470cfc7fe9223103
Aleksandr Averbukh <averbukh@google.com>
2019-09-12 00:43:40 +0200

    Add tests for busines-units example

---
+++ tests/foundations/business-units/test_outputs.py
@@ -0,0 +1,48 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"Test root module outputs."
+
+
+def test_project_ids(plan):
+  "Project ids should use prefix and match expected values."
+  prefix = plan.variables['prefix']
+  assert plan.outputs['audit_logs_project'] == prefix + '-audit'
+  assert plan.outputs['shared_resources_project'] == prefix + '-shared'
+  assert plan.outputs['terraform_project'] == prefix + '-terraform'
+
+
+def test_bucket_names(plan):
+  "GCS bucket names should use prefix and location and match expected values."
+  location = plan.variables['gcs_location'].lower()
+  prefix = plan.variables['prefix']
+  bootstrap_bucket = plan.outputs['bootstrap_tf_gcs_bucket']
+  assert bootstrap_bucket.startswith(prefix)
+  assert bootstrap_bucket.endswith('tf-bootstrap')
+  assert '-%s-' % location in bootstrap_bucket
+
+
+def test_environment_buckets(plan):
+  "One GCS bucket should be created for each environment."
+  buckets = plan.outputs['environment_tf_gcs_buckets']
+  for environment in plan.variables['environments']:
+    assert environment in buckets
+    assert buckets[environment].endswith(environment)
+
+
+def test_bq_dataset(plan):
+  "Bigquery dataset should be named in the following way 'logs_audit_<ROOT_NODE_TYPE>_<ROOT NODE NUMERIC ID>'"
+  root_node_type = plan.variables['root_node'].split("/")[0][:-1]
+  root_node_numeric_id = plan.variables['root_node'].split("/")[1]
+  assert plan.outputs['audit_logs_bq_dataset'] == 'logs_audit_' + root_node_type + '_' + root_node_numeric_id
---
+++ tests/foundations/business-units/test_projects.py
@@ -0,0 +1,44 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"Test project creation in root module."
+
+
+import pytest
+
+
+@pytest.fixture(scope='module')
+def project_modules(plan):
+  names = ['module.project-%s' %
+           name for name in ('audit', 'shared-resources', 'tf')]
+  return dict((name, plan.modules[name]) for name in names)
+
+
+def test_project_resource(plan, project_modules):
+  "Project resource attributes must match variables."
+  billing_account = plan.variables['billing_account_id']
+  project_names =  ['%s-%s' %
+           (plan.variables['prefix'], name) for name in ('shared', 'terraform', 'audit')] 
+  for name, mod in project_modules.items():
+    resource = mod['%s.google_project.project' % name]
+    assert resource['values']['billing_account'] == billing_account
+    assert resource['values']['name'] in project_names
+
+
+def test_project_services(plan, project_modules):
+  "Project service resource must enable APIs specified in the variable."
+  services = plan.variables['project_services']
+  for name, mod in project_modules.items():
+    resource = mod['%s.google_project_services.services[0]' % name]
+    assert resource['values']['services'] == services
