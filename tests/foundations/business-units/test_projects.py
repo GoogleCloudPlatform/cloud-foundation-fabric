3765efd02ffc22fbcb9c2b0157b5f7afdb30d59a
Ludovico Magnocavallo <ludomagno@google.com>
2019-10-30 22:17:00 +0100

    fix foundations/business-units tests

--- tests/foundations/business-units/test_projects.py
+++ tests/foundations/business-units/test_projects.py
@@ -31,15 +31,16 @@ def test_project_resource(plan, project_modules):
   prefix = plan.variables['prefix']
   billing_account = plan.variables['billing_account_id']
   project_names = ['%s-%s' % (prefix, name) for name in names]
-  for name, mod in project_modules.items():
-    resource = mod['%s.google_project.project' % name]
+  for mod in project_modules.values():
+    resource = mod.resources['google_project.project']
     assert resource['values']['billing_account'] == billing_account
     assert resource['values']['name'] in project_names
 
 
 def test_project_services(plan, project_modules):
   "Project service resource must enable APIs specified in the variable."
-  services = plan.variables['project_services']
-  for name, mod in project_modules.items():
-    resource = mod['%s.google_project_services.services[0]' % name]
-    assert resource['values']['services'] == services
+  num_services = len(plan.variables['project_services'])
+  for mod in project_modules.values():
+    project_services = [r for r in mod.child_modules['module.project_services'].resources if r.startswith(
+        'google_project_service.project_services')]
+    assert len(project_services) == num_services
