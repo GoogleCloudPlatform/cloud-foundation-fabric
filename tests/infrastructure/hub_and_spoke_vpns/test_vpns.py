5c7a322de9d08ccd87c2cceb1343dfba4280207f
Ludovico Magnocavallo <ludomagno@google.com>
2019-11-05 07:02:26 +0100

    streamline hub and spoke vpn test

--- tests/infrastructure/hub_and_spoke_vpns/test_vpns.py
+++ tests/infrastructure/hub_and_spoke_vpns/test_vpns.py
@@ -12,39 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"Test firewall resources creation in root module."
+"Test VPN BGP ASNs in root module."
 
 
 import pytest
 
 
-@pytest.fixture(scope='module')
-def spoke_to_hub_vpn(plan):
-  count = range(1,3)
-  names = ['module.vpn-spoke-%s-to-hub' % i for i in count]
-  return dict((name, plan.modules[name]) for name in names)
-
-
-@pytest.fixture(scope='module')
-def hub_to_spoke_vpn(plan):
-  count = range(1,3)
-  var_names = ['spoke_%s_bgp_asn' % i for i in count]
-  names = ['module.vpn-hub-to-spoke-%s' % i for i in count]
-  return var_names, [plan.modules[name] for name in names]
-
-
-def test_spokes_peer_asn(plan, spoke_to_hub_vpn):
+def test_spokes_peer_asn(plan):
   "Test that the spoke-to-hub VPNs mach input variables"
-  hub_bgp_asn = plan.variables['hub_bgp_asn']
-  for mod in spoke_to_hub_vpn.values():
-    spoke_bgp_peer = mod.resources['google_compute_router_peer.bgp_peer[0]']
-    assert spoke_bgp_peer['values']['peer_asn'] == hub_bgp_asn
+  mods = [v for k, v in plan.modules.items() if k.startswith('module.vpn-spoke')]
+  for mod in mods:
+    bgp_peer = mod.resources['google_compute_router_peer.bgp_peer[0]']
+    assert bgp_peer['values']['peer_asn'] == plan.variables['hub_bgp_asn']
 
 
-def test_hub_peer_asns(plan, hub_to_spoke_vpn):
+def test_hub_peer_asns(plan):
   "Test that the hub-to-spoke VPNs mach input variables"
-  var_names, mods = hub_to_spoke_vpn
-  for var_name, mod in zip(var_names, mods):
-    hub_bgp_peer = mod.resources['google_compute_router_peer.bgp_peer[0]']
-    spoke_bgp_asn = plan.variables[var_name]
-    assert hub_bgp_peer['values']['peer_asn'] == spoke_bgp_asn
+  mods = [v for k, v in plan.modules.items() if k.startswith('module.vpn-hub')]
+  for mod in mods:
+    bgp_peer = mod.resources['google_compute_router_peer.bgp_peer[0]']
+    asn_varname = 'spoke_%s_bgp_asn' % mod['address'][-1]
+    assert bgp_peer['values']['peer_asn'] == plan.variables[asn_varname]
