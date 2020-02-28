0614d7451792068ca56db4acbcc314840f26bd96
Aleksandr Averbukh <averbukh@google.com>
2020-02-28 15:10:25 +0100

    Refactor dynamic vpn configuration for on-prem-in-a-box module

--- docker-images/strongswan/entrypoint.sh
+++ docker-images/strongswan/entrypoint.sh
@@ -1,17 +1,30 @@
 #!/bin/sh -e
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Enable IP forwarding
 sysctl -w net.ipv4.ip_forward=1
 
-_term() {
+# Stop ipsec when terminating
+_stop_ipsec() {
   echo "Shutting down strongSwan/ipsec..."
   ipsec stop
 }
+trap _stop_ipsec SIGTERM
 
-# SIGTERM
-trap _term SIGTERM
-
+# Start ipsec
 echo "Starting up strongSwan/ipsec..."
 ipsec start --nofork "$@" &
 child=$!
-# wait for child to exit
 wait "$child"
--- modules/on-prem-in-a-box/main.tf
+++ modules/on-prem-in-a-box/main.tf
@@ -1,3 +1,9 @@
+resource "google_compute_address" "static" {
+  name = "on-prem-in-a-box"
+  region = substr(var.zone, 0, length(var.zone)-2)
+  address_type = "EXTERNAL"
+}
+
 resource "google_compute_instance" "on_prem_in_a_box" {
   project      = var.project_id
   name         = var.name
@@ -12,8 +18,10 @@ resource "google_compute_instance" "on_prem_in_a_box" {
   }
 
   network_interface {
-    subnetwork    = var.subnet
-    access_config = {}
+    subnetwork = var.subnet
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
   }
 
   metadata {
@@ -22,19 +30,26 @@ resource "google_compute_instance" "on_prem_in_a_box" {
 }
 
 data "template_file" "vpn-gw" {
-  template = file(format("$s/assets/$s-vpn-gw-cloud-init.yaml", path.module, var.gateway_type)
+  template = file(format("$s/assets/$s-vpn-gw-cloud-init.yaml", path.module, var.gateway_type))
 
   vars = {
-    dns_ip_address       = cidrhost(var.local_ip_cidr_range, 3)
-    instance_name        = var.name
-    local_ip_cidr_range  = var.local_ip_cidr_range
-    peer_ip              = var.peer_ip
-    peer_ip_wildcard     = format("%%%s", var.peer_ip)
-    remote_ip_cidr_range = var.remote_ip_cidr_range
-    shared_secret        = var.shared_secret
-    server_config_script = indent(4, file(format("$s/assets/server-config.sh", path.module)))
-    vpn_ip_address       = cidrhost(var.local_ip_cidr_range, 2)
-    web_ip_address       = cidrhost(var.local_ip_cidr_range, 4)
+    instance_name           = var.name
+    local_ip_cidr_range     = var.local_ip_cidr_range
+    peer_ip                 = var.peer_ip
+    remote_ip_cidr_range    = var.remote_ip_cidr_ranges
+    shared_secret           = var.shared_secret
+    server_config_script    = indent(4, file(format("$s/assets/server-config.sh", path.module)))
+    local_gw_ip             = cidrhost(var.local_ip_cidr_range, 1)
+    vpn_ip_address          = cidrhost(var.local_ip_cidr_range, 2)
+    dns_ip_address          = cidrhost(var.local_ip_cidr_range, 3)
+    web_ip_address          = cidrhost(var.local_ip_cidr_range, 4)
+    toolbox_ip_address      = cidrhost(var.local_ip_cidr_range, 5)
+    peer_bgp_session_range  = var.peer_bgp_session_range
+    local_bgp_session_range = var.local_bgp_session_range
+    peer_bgp_ip             = split(var.peer_bgp_session_range, "/")[0]
+    local_bgp_ip            = split(var.local_bgp_session_range, "/")[0]
+    peer_bgp_asn            = var.peer_bgp_asn
+    local_bgp_asn           = var.local_bgp_asn
   }
 }
 
