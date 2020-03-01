0efc69b0ca39218efa22b4efefde7e747707195f
Aleksandr Averbukh <averbukh@google.com>
2020-03-02 00:08:01 +0100

    Add static vpn gw to on-prem-in-a-box module

--- modules/on-prem-in-a-box/main.tf
+++ modules/on-prem-in-a-box/main.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 resource "google_compute_address" "static" {
   project      = var.project_id
   name         = var.name
@@ -14,7 +30,7 @@ resource "google_compute_instance" "on_prem_in_a_box" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
 
@@ -29,7 +45,6 @@ resource "google_compute_instance" "on_prem_in_a_box" {
     user-data = data.template_file.vpn-gw.rendered
   }
 
-  metadata_startup_script = file(format("%s/assets/server-config.sh", path.module))
 }
 
 data "template_file" "vpn-gw" {
--- modules/on-prem-in-a-box/variables.tf
+++ modules/on-prem-in-a-box/variables.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 variable "project_id" {
   type        = string
   description = "Project id."
@@ -38,7 +54,7 @@ variable "network_tags" {
 
 variable "vpn_gateway_type" {
   type        = string
-  description = "VPN Gateway type, applicable values are `static` and `dynamic`. Input `remote_ip_cidr_ranges` should be provided in case of `static` vpn gateway. "
+  description = "VPN Gateway type, applicable values are `static` and `dynamic`."
 }
 
 
@@ -49,25 +65,25 @@ variable "peer_ip" {
 
 variable "peer_bgp_session_range" {
   type        = string
-  description = "Peer BGP sesison range of the BGP interface."
+  description = "Peer BGP sesison range of the BGP interface. Should be provided if `vpn_gateway_type` is `dynamic`"
   default     = "169.254.0.1/30"
 }
 
 variable "local_bgp_session_range" {
   type        = string
-  description = "Local BGP sesison range of the BGP interface."
+  description = "Local BGP sesison range of the BGP interface. Should be provided if `vpn_gateway_type` is `dynamic`"
   default     = "169.254.0.2/30"
 }
 
 variable "peer_bgp_asn" {
   type        = string
-  description = "Peer BGP ASN."
+  description = "Peer BGP ASN. Should be provided if `vpn_gateway_type` is `dynamic`"
   default     = "65001"
 }
 
 variable "local_bgp_asn" {
   type        = string
-  description = "Local BGP ASN."
+  description = "Local BGP ASN. Should be provided if `vpn_gateway_type` is `dynamic`"
   default     = "65002"
 }
 
@@ -77,12 +93,11 @@ variable "local_ip_cidr_range" {
 }
 
 variable "remote_ip_cidr_ranges" {
-  description = "List of comma separated remote CIDR ranges"
+  description = "List of comma separated remote CIDR ranges. Should be provided if `vpn_gateway_type` is `static`"
   type        = string
   default     = ""
 }
 
 variable "shared_secret" {
   type    = string
-  default = ""
 }
