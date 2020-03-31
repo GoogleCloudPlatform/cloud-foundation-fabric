ee6b05658393fdfdb85610bb7983344957565947
Ludovico Magnocavallo <ludomagno@google.com>
2020-03-31 21:03:40 +0200

    cos mysql tests

---
+++ tests/modules/cos_container_mysql/test_apply.py
@@ -0,0 +1,50 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import pytest
+import re
+import yaml
+
+
+FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixture')
+
+
+def test_defaults(apply_runner):
+  "Test defalt configuration."
+  _, output = apply_runner(FIXTURES_DIR, mysql_password='foo')
+  cloud_config = output['cloud_config']
+  yaml.safe_load(cloud_config)
+  assert cloud_config.startswith('#cloud-config')
+  assert re.findall(r'(?m)^\s+\-\s*path:\s*(\S+)', cloud_config) == [
+      '/var/lib/docker/daemon.json', '/run/secrets/mysql-passwd-orig.txt',
+      '/run/mysql-passwd.sh', '/run/mysql/conf.d/my.cnf',
+      '/etc/systemd/system/mysql-data.service',
+      '/etc/systemd/system/mysql.service'
+  ]
+  assert 'gcloud' not in cloud_config
+
+
+def test_kms(apply_runner):
+  "Test KMS configuration."
+  kms_config = (
+      '{project_id="my-project", keyring="my-keyring", location="eu", key="foo"}'
+  )
+  _, output = apply_runner(
+      FIXTURES_DIR, mysql_password='foo', kms_config=kms_config)
+  cloud_config = output['cloud_config']
+  yaml.safe_load(cloud_config)
+  assert cloud_config.startswith('#cloud-config')
+  assert 'gcloud' in cloud_config
