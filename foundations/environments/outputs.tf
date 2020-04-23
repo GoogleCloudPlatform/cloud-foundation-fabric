41e8e70e4f56c73058a91aa0b8f3ca334ddecdb6
Aleksandr Averbukh <averbukh@google.com>
2020-04-23 07:10:52 +0200

    Update outputs for foundations/environments example (#59)

--- foundations/environments/outputs.tf
+++ foundations/environments/outputs.tf
@@ -12,52 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-/* output "terraform_project" {
+output "terraform_project" {
   description = "Project that holds the base Terraform resources."
-  value       = module.project-tf.project_id
+  value       = module.tf-project.project_id
 }
 
 output "bootstrap_tf_gcs_bucket" {
   description = "GCS bucket used for the bootstrap Terraform state."
-  value       = module.gcs-tf-bootstrap.name
+  value       = module.tf-gcs-bootstrap.name
 }
 
 output "environment_folders" {
   description = "Top-level environment folders."
-  value       = module.folders-top-level.ids
+  value       = module.environment-folders.ids
 }
 
 output "environment_tf_gcs_buckets" {
   description = "GCS buckets used for each environment Terraform state."
-  value       = module.gcs-tf-environments.names
+  value       = module.tf-gcs-environments.names
 }
 
 output "environment_service_account_keys" {
   description = "Service account keys used to run each environment Terraform modules."
   sensitive   = true
-  value       = module.service-accounts-tf-environments.keys
+  value       = module.tf-service-accounts.keys
 }
 
 output "environment_service_accounts" {
   description = "Service accounts used to run each environment Terraform modules."
-  value       = module.service-accounts-tf-environments.emails
+  value       = module.tf-service-accounts.emails
 }
 
 output "audit_logs_bq_dataset" {
   description = "Bigquery dataset for the audit logs export."
-  value       = module.bq-audit-export.resource_name
+  value       = module.audit-datasets.datasets[0].dataset_id
 }
 
 output "audit_logs_project" {
   description = "Project that holds the audit logs export resources."
-  value       = module.project-audit.project_id
+  value       = module.audit-project.project_id
 }
 
-output "shared_resources_project" {
+output "shared_services_project" {
   description = "Project that holdes resources shared across environments."
-  value       = module.project-shared-resources.project_id
+  value       = module.sharedsvc-project.project_id
 }
 
 # Add further outputs here for the additional modules that manage shared
 # resources, like GCR, GCS buckets, KMS, etc.
- */
