ae74f4da67698bb58a879d85a6092dc09b79a8f5
Ludovico Magnocavallo <ludomagno@google.com>
2020-08-14 11:38:50 +0200

    add basic plan tests for all networking e2e examples

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,21 +23,34 @@ BASEDIR = os.path.dirname(os.path.dirname(__file__))
 
 
 @pytest.fixture(scope='session')
+def e2e_plan_runner(plan_runner):
+  "Returns a function to run Terraform plan on an end-to-end fixture."
+
+  def run_plan(fixture_path, targets=None):
+    "Runs Terraform plan on an end-to-end module using defaults, returns data."
+    _, modules = plan_runner(fixture_path, is_module=False, targets=targets)
+    resources = [r for m in modules.values() for r in m]
+    return modules, resources
+
+  return run_plan
+
+
+@pytest.fixture(scope='session')
 def plan_runner():
   "Returns a function to run Terraform plan on a fixture."
 
-  def run_plan(fixture_path, is_module=True, **tf_vars):
+  def run_plan(fixture_path, is_module=True, targets=None, **tf_vars):
     "Runs Terraform plan and returns parsed output"
     tf = tftest.TerraformTest(fixture_path, BASEDIR,
                               os.environ.get('TERRAFORM', 'terraform'))
     tf.setup()
-    plan = tf.plan(output=True, tf_vars=tf_vars)
+    plan = tf.plan(output=True, tf_vars=tf_vars, targets=targets)
     root_module = plan.planned_values['root_module']['child_modules'][0]
     if is_module:
       return (plan, root_module['resources'])
     modules = dict((mod['address'], mod['resources'])
                    for mod in root_module['child_modules'])
-    return (plan, modules)
+    return plan, modules
 
   return run_plan
 
@@ -53,6 +66,6 @@ def apply_runner():
     tf.setup()
     apply = tf.apply(tf_vars=tf_vars)
     output = tf.output(json_format=True)
-    return (apply, output)
+    return apply, output
 
   return run_apply
