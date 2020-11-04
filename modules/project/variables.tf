8dc131550f2f0ceb4e75461f3dc6af26ada35607
Ludovico Magnocavallo <ludomagno@google.com>
2020-11-04 12:06:01 +0100

    fix variable validation in project module to allow for null values

--- modules/project/variables.tf
+++ modules/project/variables.tf
@@ -84,7 +84,7 @@ variable "parent" {
   type        = string
   default     = null
   validation {
-    condition     = can(regex("(organizations|folders)/[0-9]+", var.parent))
+    condition     = var.parent == null || can(regex("(organizations|folders)/[0-9]+", var.parent))
     error_message = "Parent must be of the form folders/folder_id or organizations/organization_id."
   }
 }
