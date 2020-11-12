80acb607c24bd108e73692ed7ccd61b512211787
Ludovico Magnocavallo <ludomagno@google.com>
2020-11-12 13:57:56 +0100

    Add NLB health checks source ranges
    https://cloud.google.com/load-balancing/docs/health-checks#fw-netlb

--- modules/net-vpc-firewall/variables.tf
+++ modules/net-vpc-firewall/variables.tf
@@ -45,13 +45,13 @@ variable "ssh_source_ranges" {
 variable "http_source_ranges" {
   description = "List of IP CIDR ranges for tag-based HTTP rule, defaults to the health checkers ranges."
   type        = list(string)
-  default     = ["35.191.0.0/16", "130.211.0.0/22"]
+  default     = ["35.191.0.0/16", "130.211.0.0/22", "209.85.152.0/22", "209.85.204.0/22"]
 }
 
 variable "https_source_ranges" {
   description = "List of IP CIDR ranges for tag-based HTTPS rule, defaults to the health checkers ranges."
   type        = list(string)
-  default     = ["35.191.0.0/16", "130.211.0.0/22"]
+  default     = ["35.191.0.0/16", "130.211.0.0/22", "209.85.152.0/22", "209.85.204.0/22"]
 }
 
 variable "custom_rules" {
