f611e99e469724caeab8f76a04573ebcabe223ae
Ludovico Magnocavallo <ludomagno@google.com>
2020-11-19 16:48:08 +0100

    add mtu (#170)

--- modules/net-vpc/main.tf
+++ modules/net-vpc/main.tf
@@ -88,6 +88,7 @@ resource "google_compute_network" "network" {
   description                     = var.description
   auto_create_subnetworks         = var.auto_create_subnetworks
   delete_default_routes_on_create = var.delete_default_routes_on_create
+  mtu                             = var.mtu
   routing_mode                    = var.routing_mode
 }
 
