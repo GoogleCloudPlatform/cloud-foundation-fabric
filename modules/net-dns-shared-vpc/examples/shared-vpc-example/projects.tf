ed7071e6c3875d5eeb95f21a42e2dad9678d4701
Aurélien Legrand <aurelien.legrand01@gmail.com>
2021-01-08 11:07:00 +0000

    Removing dead code.

--- modules/net-dns-shared-vpc/examples/shared-vpc-example/projects.tf
+++ modules/net-dns-shared-vpc/examples/shared-vpc-example/projects.tf
@@ -71,18 +71,3 @@ module "project-service-2" {
     host_project = module.project-host.project_id
   }
 }
-
-/*
-locals {
-  # Adding prefix and random ID before the service project names to generate the service project IDs
-  service_projects = [for name in var.service_projects[*] : "${var.prefix}-${random_id.id.hex}-${name}"]
-}
-
-module "net-shared-vpc-access" {
-  source              = "terraform-google-modules/network/google//modules/fabric-net-svpc-access"
-  version             = "~> 2.6"
-  host_project_id     = module.project-host.project_id
-  service_project_num = length(local.service_projects)
-  service_project_ids = local.service_projects
-}
-*/
