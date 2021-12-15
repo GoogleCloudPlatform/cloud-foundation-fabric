601ebd028e9d77d699dc0816596d916faa0d7760
Stenio Ferreira <stenio123+developer@gmail.com>
2021-12-15 07:56:53 -0600

    Added support for Stateful Managed Instance Groups (#367)
    * First iteration updates
    
    * All tests passing
    
    * Updated README and var descriptions
    
    * Updated README
    
    * Updated example README
    
    * Consolidated stateful vars
    
    * consolidated stateful vars
    
    * Updated README
    
    * Requested changes to try
    
    * Fixed README examples and try
    
    Co-authored-by: Ludovico Magnocavallo <ludomagno@google.com>

--- tests/modules/compute_mig/test_plan.py
+++ tests/modules/compute_mig/test_plan.py
@@ -24,6 +24,7 @@ def test_defaults(plan_runner):
   "Test variable defaults."
   _, resources = plan_runner(FIXTURES_DIR)
   assert len(resources) == 1
+  print(resources[0]['type'])
   mig = resources[0]
   assert mig['type'] == 'google_compute_instance_group_manager'
   assert mig['values']['target_size'] == 2
@@ -34,7 +35,6 @@ def test_defaults(plan_runner):
   assert mig['type'] == 'google_compute_region_instance_group_manager'
   assert mig['values']['target_size'] == 2
   assert mig['values']['region']
-
 
 def test_health_check(plan_runner):
   "Test health check resource."
@@ -75,3 +75,81 @@ def test_autoscaler(plan_runner):
   assert len(resources) == 2
   autoscaler = resources[0]
   assert autoscaler['type'] == 'google_compute_region_autoscaler'
+  
+def test_stateful_mig(plan_runner):
+  "Test stateful instances - mig."
+
+  stateful_config = (
+    '{'
+      'per_instance_config = {},'
+      'mig_config = {'
+        'stateful_disks = {'
+          'persistent-disk-1 = {delete_rule="NEVER"}'
+        '}' 
+      '}'
+    '}'
+  )
+  _, resources = plan_runner(
+      FIXTURES_DIR, stateful_config=stateful_config)
+  assert len(resources) == 1
+  statefuldisk = resources[0]
+  assert statefuldisk['type'] == 'google_compute_instance_group_manager'
+  assert statefuldisk['values']['stateful_disk'] == [{
+      'device_name': 'persistent-disk-1',
+      'delete_rule': 'NEVER',
+  }]
+  
+def test_stateful_instance(plan_runner):
+  "Test stateful instances - instance."
+  stateful_config = (
+    '{'
+      'per_instance_config = {'
+        'instance-1 = {'
+          'stateful_disks = {'
+            'persistent-disk-1 = {'
+              'source = "test-disk",' 
+              'mode = "READ_ONLY",'
+              'delete_rule= "NEVER",'
+            '},'
+          '},'
+          'metadata = {'
+            'foo = "bar"'
+          '},'
+          'update_config = {'
+            'minimal_action                   = "NONE",'
+            'most_disruptive_allowed_action   = "REPLACE",'
+            'remove_instance_state_on_destroy = false,'
+            
+          '},'
+        '},'
+      '},'
+      'mig_config = {'
+        'stateful_disks = {'
+          'persistent-disk-1 = {delete_rule="NEVER"}'
+        '}' 
+      '}'
+    '}'
+  )
+  _, resources = plan_runner(
+      FIXTURES_DIR, stateful_config=stateful_config)
+  assert len(resources) == 2
+  instanceconfig = resources[0]
+  assert instanceconfig['type'] == 'google_compute_instance_group_manager'
+  instanceconfig = resources[1]
+  assert instanceconfig['type'] == 'google_compute_per_instance_config'
+  
+  assert instanceconfig['values']['preserved_state'] == [{
+      'disk': [{
+        'device_name': 'persistent-disk-1',
+        'delete_rule': 'NEVER',
+        'source': 'test-disk',
+        'mode': 'READ_ONLY',
+      }],
+      'metadata': {
+        'foo': 'bar'
+      }
+  }]
+
+  assert instanceconfig['values']['minimal_action'] == 'NONE'
+  assert instanceconfig['values']['most_disruptive_allowed_action'] == 'REPLACE'
+  assert instanceconfig['values']['remove_instance_state_on_destroy'] == False
