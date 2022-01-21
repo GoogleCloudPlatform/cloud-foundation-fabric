487ac1d82621379303a8c5fba03caa68bd1bad9c
Lorenzo Caggioni <lorenzo.caggioni@gmail.com>
2022-01-21 20:39:17 +0100

    Fix tests

--- tests/examples/data_solutions/gcs_to_bq_with_least_privileges/test_plan.py
+++ tests/examples/data_solutions/gcs_to_bq_with_least_privileges/test_plan.py
@@ -24,4 +24,4 @@ def test_resources(e2e_plan_runner):
   "Test that plan works and the numbers of resources is as expected."
   modules, resources = e2e_plan_runner(FIXTURES_DIR)
   assert len(modules) == 11
-  assert len(resources) == 43
+  assert len(resources) == 44
