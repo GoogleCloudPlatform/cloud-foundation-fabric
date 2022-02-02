b2cb55a58f744bed6b860568d4af1782846b178f
Lorenzo Caggioni <lorenzo.caggioni@gmail.com>
2022-02-02 16:55:19 +0100

    Fix test, for real? :-)

--- tests/examples/data_solutions/dp-foundation/test_plan.py
+++ tests/examples/data_solutions/dp-foundation/test_plan.py
@@ -24,4 +24,4 @@ def test_resources(e2e_plan_runner):
   "Test that plan works and the numbers of resources is as expected."
   modules, resources = e2e_plan_runner(FIXTURES_DIR)
   assert len(modules) == 40
-  assert len(resources) == 281
+  assert len(resources) == 286
