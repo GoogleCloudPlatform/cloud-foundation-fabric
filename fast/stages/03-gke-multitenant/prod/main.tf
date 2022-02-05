d4c398a8317cad61a4d6f5e2aefa32af06e20314
Julio Castillo <jccb@google.com>
2022-02-05 16:59:07 +0100

    Remove old test project

--- fast/stages/03-gke-multitenant/prod/main.tf
+++ fast/stages/03-gke-multitenant/prod/main.tf
@@ -66,47 +66,6 @@ module "gke-project-0" {
   # }
 }
 
-module "gke-project-1" {
-  source          = "../../../../modules/project"
-  billing_account = var.billing_account_id
-  name            = "${var.environment}-gke-clusters-2j"
-  parent          = var.folder_id
-  prefix          = var.prefix
-  labels          = local.labels
-  services = [
-    "container.googleapis.com",
-    "dns.googleapis.com",
-    "stackdriver.googleapis.com",
-    # uncomment if you need Multi-cluster Ingress / Gateway API
-    # "gkehub.googleapis.com",
-    # "multiclusterservicediscovery.googleapis.com",
-    # "multiclusteringress.googleapis.com",
-    # "trafficdirector.googleapis.com"
-  ]
-  # add here any other service ids and keys for robot accounts which are needed
-  # service_encryption_key_ids = {
-  #   container = var.project_config.service_encryption_key_ids
-  # }
-  shared_vpc_service_config = {
-    attach       = true
-    host_project = var.vpc_host_project
-  }
-  # specify project-level org policies here if you need them
-
-  # policy_boolean = {
-  #   "constraints/compute.disableGuestAttributesAccess" = true
-  # }
-  # policy_list = {
-  #   "constraints/compute.trustedImageProjects" = {
-  #     inherit_from_parent = null
-  #     suggested_value     = null
-  #     status              = true
-  #     values              = ["projects/fl01-prod-iac-core-0"]
-  #   }
-  # }
-}
-
-
 module "gke-dataset-resource-usage" {
   source        = "../../../../modules/bigquery-dataset"
   project_id    = module.gke-project-0.project_id
