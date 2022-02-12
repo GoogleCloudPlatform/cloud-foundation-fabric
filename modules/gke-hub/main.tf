9aa1bb338dbc098e6137a226fd1540144729e58a
Daniel Marzini <dmarzi@google.com>
2022-02-12 22:07:01 +0100

    variable management adjust

--- modules/gke-hub/main.tf
+++ modules/gke-hub/main.tf
@@ -22,38 +22,39 @@
 
 locals {
 
+
+  config_sync       = coalesce(var.hub_config.config_sync, var.config_sync_defaults)
+  policy_controller = coalesce(var.hub_config.policy_controller, var.policy_controller_defaults)
   hub_config = {
-    for k, config in var.hub_config :
-    k => merge(config, {
-      config_sync       = coalesce(config.config_sync, var.config_sync_defaults)
-      policy_controller = coalesce(config.policy_controller, var.policy_controller_defaults)
-    })
+    clusters          = var.hub_config.clusters
+    config_sync       = merge(var.hub_config.config_sync, local.config_sync)
+    policy_controller = merge(var.hub_config.policy_controller, local.policy_controller)
   }
 
   config_sync_sa_email = (
-    var.hub_config.config_sync.workload_identity_sa == null
+    local.hub_config.config_sync.workload_identity_sa == null
     ? (
       length(module.gke-workload-identity-platform-sa[0]) > 0
       ? module.gke-workload-identity-platform-sa[0].email
       : null
     )
-    : var.hub_config.config_sync.workload_identity_sa
+    : local.hub_config.config_sync.workload_identity_sa
   )
   config_sync_repository_url = (
-    var.hub_config.config_sync.repository_url == null
+    local.hub_config.config_sync.repository_url == null
     ? (
       length(module.gke-config-management-platform-repo[0]) > 0
       ? module.gke-config-management-platform-repo[0].url
       : null
     )
-    : var.hub_config.config_sync.repository_url
+    : local.hub_config.config_sync.repository_url
   )
 }
 
 # add the cluster to the gke hub
 resource "google_gke_hub_membership" "membership" {
   provider      = google-beta
-  for_each      = { for i, v in var.hub_config.clusters : i => v }
+  for_each      = { for i, v in local.hub_config.clusters : i => v }
   membership_id = each.value.name
   project       = var.project_id
   endpoint {
@@ -65,7 +66,7 @@ resource "google_gke_hub_membership" "membership" {
 
 #enable configmanagement feature on the the clusters registered into the hub
 resource "google_gke_hub_feature" "feature" {
-  count    = var.hub_config.config_sync == null ? 0 : 1
+  count    = local.hub_config.config_sync == null ? 0 : 1
   provider = google-beta
   project  = var.project_id
   name     = "configmanagement"
@@ -74,7 +75,7 @@ resource "google_gke_hub_feature" "feature" {
 
 # sa used by configsync, via WorkloadID to pull code from the repo
 module "gke-workload-identity-platform-sa" {
-  count        = var.hub_config.config_sync == null ? 1 : 0 #TOFIX .workload_identity_sa
+  count        = local.hub_config.config_sync.workload_identity_sa == null ? 1 : 0
   source       = "../../modules/iam-service-account"
   project_id   = var.project_id
   name         = "gke-wid-platform-sa"
@@ -86,7 +87,7 @@ module "gke-workload-identity-platform-sa" {
 
 # # #source repository
 module "gke-config-management-platform-repo" {
-  count      = var.hub_config.config_sync == null ? 1 : 0 #TOFIX .repository_url
+  count      = local.hub_config.config_sync.repository_url == null ? 1 : 0
   source     = "../../modules/source-repository"
   project_id = var.project_id
   name       = "gke-config-management-platform-repo"
@@ -95,42 +96,38 @@ module "gke-config-management-platform-repo" {
   }
 }
 
-# configure configmanagement feature for the hub
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/gke_hub_feature_membership
+# # configure configmanagement feature for the hub
+# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/gke_hub_feature_membership
 resource "google_gke_hub_feature_membership" "feature_member" {
   provider = google-beta
   project  = var.project_id
 
-  for_each   = { for i, v in var.hub_config.clusters : i => v }
+  for_each   = { for i, v in local.hub_config.clusters : i => v }
   location   = "global"
   feature    = google_gke_hub_feature.feature[0].name
-  membership = google_gke_hub_membership.membership[each.value.name].membership_id
+  membership = google_gke_hub_membership.membership[each.key].membership_id
   configmanagement {
     version = "1.10.0"
-    # enabled = true
 
-    dynamic "config_sync" {
-      for_each = var.hub_config.config_sync
-      content {
-        git {
-          sync_repo                 = local.config_sync_repository_url
-          sync_branch               = var.hub_config.config_sync.repository_branch
-          secret_type               = "gcpserviceaccount" #TOFIX
-          gcp_service_account_email = local.config_sync_sa_email
-          policy_dir                = var.hub_config.config_sync.repository_policy_dir
-        }
-        source_format = var.hub_config.config_sync.repository_source_format #"hierarchy"
+    config_sync {
+      git {
+        sync_repo                 = local.config_sync_repository_url
+        sync_branch               = local.hub_config.config_sync.repository_branch
+        secret_type               = "gcpserviceaccount" #TOFIX
+        gcp_service_account_email = local.config_sync_sa_email
+        policy_dir                = local.hub_config.config_sync.repository_policy_dir
       }
+      source_format = local.hub_config.config_sync.repository_source_format #"hierarchy"
     }
 
-    dynamic "policy_controller" {
-      for_each = var.hub_config.policy_controller
-      content {
-        enabled                    = true
-        log_denies_enabled         = var.hub_config.policy_controller.enable_log_denies
-        exemptable_namespaces      = var.hub_config.policy_controller.exemptable_namespaces
-        template_library_installed = var.hub_config.policy_controller.enable_template_library # https://cloud.google.com/anthos-config-management/docs/reference/constraint-template-library
-      }
+    policy_controller {
+      enabled                    = true
+      log_denies_enabled         = local.hub_config.policy_controller.enable_log_denies
+      exemptable_namespaces      = local.hub_config.policy_controller.exemptable_namespaces
+      template_library_installed = local.hub_config.policy_controller.enable_template_library # https://cloud.google.com/anthos-config-management/docs/reference/constraint-template-library
+
     }
+
+
   }
 }
