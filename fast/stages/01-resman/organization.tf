04f054478daa5d4edba25679b22e4d15494eac24
Lorenzo Caggioni <lorenzo.caggioni@gmail.com>
2022-02-18 00:34:51 +0100

    Fix poliscy admin role

--- fast/stages/01-resman/organization.tf
+++ fast/stages/01-resman/organization.tf
@@ -63,7 +63,10 @@ module "organization" {
       "roles/compute.xpnAdmin" = [
         module.branch-network-sa.iam_email
       ]
-      "roles/orgpolicy.policyAdmin" = local.branch_teams_pf_sa_iam_emails
+      "roles/orgpolicy.policyAdmin" = concat(
+        local.branch_dataplatform_sa_iam_emails,
+        local.branch_teams_pf_sa_iam_emails
+      )
     },
     local.billing_org ? {
       "roles/billing.costsManager" = local.branch_teams_pf_sa_iam_emails
