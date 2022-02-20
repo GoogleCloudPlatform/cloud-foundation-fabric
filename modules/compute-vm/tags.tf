758be3f66cb8986a995fd142acdeb8988bd90b63
Ludovico Magnocavallo <ludomagno@google.com>
2022-02-20 08:26:50 +0100

    fix compute-vm

--- modules/compute-vm/tags.tf
+++ modules/compute-vm/tags.tf
@@ -15,7 +15,7 @@
  */
 
 resource "google_tags_tag_binding" "binding" {
-  for_each  = coalesce(var.tag_bindings, {})
-  parent    = "//compute.googleapis.com/${google_compute_instance.default.id}"
+  for_each  = var.create_template ? {} : coalesce(var.tag_bindings, {})
+  parent    = "//compute.googleapis.com/${google_compute_instance.default.0.id}"
   tag_value = each.value
 }
