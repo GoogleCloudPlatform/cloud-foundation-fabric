fdf585fffe88346954bf00c4487c045b4177380a
Julio Castillo <jccb@google.com>
2022-02-28 11:48:22 +0100

    Add gke-hub tests

---
+++ tests/modules/gke_hub/fixture/main.tf
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "hub" {
+  source          = "../../../../modules/gke-hub"
+  project_id      = var.project_id
+  member_clusters = var.member_clusters
+  member_features = {
+    configmanagement = {
+      binauthz = true
+      config_sync = {
+        gcp_service_account_email = null
+        https_proxy               = null
+        policy_dir                = "configsync"
+        secret_type               = "none"
+        source_format             = "hierarchy"
+        sync_branch               = "main"
+        sync_repo                 = "https://github.com/danielmarzini/configsync-platform-example"
+        sync_rev                  = null
+      }
+      hierarchy_controller = {
+        enable_hierarchical_resource_quota = true
+        enable_pod_tree_labels             = true
+      }
+      policy_controller = {
+        exemptable_namespaces      = []
+        log_denies_enabled         = true
+        referential_rules_enabled  = true
+        template_library_installed = true
+      }
+      version = "1.10.2"
+    }
+  }
+}
