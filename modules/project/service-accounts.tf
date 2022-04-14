29e1b59e42db6dbc103bc27a84a8467f877bb67d
Ludovico Magnocavallo <ludomagno@google.com>
2022-04-15 01:48:24 +0200

    linting

--- modules/project/service-accounts.tf
+++ modules/project/service-accounts.tf
@@ -39,13 +39,13 @@ locals {
     dataproc          = "service-%s@dataproc-accounts"
     gae-flex          = "service-%s@gae-api-prod"
     # TODO: deprecate gcf
-    gcf           = "service-%s@gcf-admin-robot"
+    gcf                      = "service-%s@gcf-admin-robot"
     monitoring-notifications = "service-%s@gcp-sa-monitoring-notification"
-    pubsub        = "service-%s@gcp-sa-pubsub"
-    secretmanager = "service-%s@gcp-sa-secretmanager"
-    sql           = "service-%s@gcp-sa-cloud-sql"
-    sqladmin      = "service-%s@gcp-sa-cloud-sql"
-    storage       = "service-%s@gs-project-accounts"
+    pubsub                   = "service-%s@gcp-sa-pubsub"
+    secretmanager            = "service-%s@gcp-sa-secretmanager"
+    sql                      = "service-%s@gcp-sa-cloud-sql"
+    sqladmin                 = "service-%s@gcp-sa-cloud-sql"
+    storage                  = "service-%s@gs-project-accounts"
   }
   service_accounts_default = {
     compute = "${local.project.number}-compute@developer.gserviceaccount.com"
