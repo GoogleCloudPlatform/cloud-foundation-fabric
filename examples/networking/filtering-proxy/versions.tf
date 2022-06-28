07b00c34fe07bef91145a1309107ebbc193dc03a
Julio Castillo <jccb@google.com>
2022-06-28 09:49:54 +0200

    Bump providers versions and pin versions for tests

--- examples/networking/filtering-proxy/versions.tf
+++ examples/networking/filtering-proxy/versions.tf
@@ -17,11 +17,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.17.0"
+      version = ">= 4.20.0" # tftest
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.17.0"
+      version = ">= 4.20.0" # tftest
     }
   }
 }
