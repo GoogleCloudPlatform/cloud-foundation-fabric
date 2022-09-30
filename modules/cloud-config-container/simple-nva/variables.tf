5950797385de6157e1c9e75007b6ae5742d9938d
Simone Ruffilli <sruffilli@google.com>
2022-09-30 18:43:24 +0200

    COS based simple networking appliance (#846)

---
+++ modules/cloud-config-container/simple-nva/variables.tf
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "cloud_config" {
+  description = "Cloud config template path. If null default will be used."
+  type        = string
+  default     = null
+}
+
+variable "enable_health_checks" {
+  description = "Configures routing to enable responses to health check probes."
+  type        = bool
+  default     = false
+}
+
+variable "network_interfaces" {
+  description = "Network interfaces configuration."
+  type = list(object({
+    routes = optional(list(string))
+  }))
+}
