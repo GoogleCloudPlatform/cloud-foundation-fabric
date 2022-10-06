1955a611591f2836b12d3b7f58e355fcfc3603ff
Julio Castillo <jccb@google.com>
2022-10-06 15:48:37 +0200

    Make project shared vpc fields optional

--- modules/project/variables.tf
+++ modules/project/variables.tf
@@ -237,7 +237,7 @@ variable "shared_vpc_host_config" {
   description = "Configures this project as a Shared VPC host project (mutually exclusive with shared_vpc_service_project)."
   type = object({
     enabled          = bool
-    service_projects = list(string)
+    service_projects = optional(list(string), [])
   })
   default = null
 }
@@ -247,7 +247,7 @@ variable "shared_vpc_service_config" {
   # the list of valid service identities is in service-accounts.tf
   type = object({
     host_project         = string
-    service_identity_iam = map(list(string))
+    service_identity_iam = optional(map(list(string)))
   })
   default = null
 }
