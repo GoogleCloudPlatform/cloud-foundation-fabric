af447e34e6e729aca6a85d7b44d060373d89e729
Ludovico Magnocavallo <ludomagno@google.com>
2022-10-14 19:14:47 +0200

    Improve handling of tf plugin cache in tests (#885)
    * disable tf cache in CI
    
    * re-enable cache
    
    * try a different approach

--- default-versions.tf
+++ default-versions.tf
@@ -23,14 +23,7 @@ terraform {
       source  = "hashicorp/google-beta"
       version = ">= 4.36.0" # tftest
     }
-    # used in modules
-    archive = {
-      source = "hashicorp/archive"
-    }
-    # used in fast
-    local = {
-      source = "hashicorp/local"
-    }
+    # CI hook
   }
 }
 
---
+++ tests/test-versions.tf
@@ -0,0 +1,4 @@
+# skip boilerplate check
+archive = { source = "hashicorp/archive" }
+local   = { source = "hashicorp/local" }
+random  = { source = "hashicorp/random" }
