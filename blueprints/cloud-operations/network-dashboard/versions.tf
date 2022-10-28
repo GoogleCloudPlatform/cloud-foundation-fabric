22736473fdd41dadbd264ce8c1f4097d72fa0a2c
Brian Jung <65934595+brianhmj@users.noreply.github.com>
2022-10-28 09:22:59 -0400

    Update versions.tf

--- blueprints/cloud-operations/network-dashboard/versions.tf
+++ blueprints/cloud-operations/network-dashboard/versions.tf
@@ -17,7 +17,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.40.0"
+      version = ">= 4.40.0" # tftest
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.40.0" # tftest
     }
   }
 }
