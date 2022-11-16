5f710f21dc060a5a81db04ac16fbe4c4b5484f73
Ludovico Magnocavallo <ludomagno@google.com>
2022-11-16 14:14:05 +0100

    Add support for serverless NEGs to ILB L7 module (#983)
    * refactor neg variable for serverless
    
    * cloudrun negs
    
    * cloudrun negs
    
    * serverless neg test
    
    * re-enable forwarding rule output

--- modules/net-ilb-l7/main.tf
+++ modules/net-ilb-l7/main.tf
@@ -16,7 +16,7 @@
 
 locals {
   _neg_endpoints = flatten([
-    for k, v in var.neg_configs : [
+    for k, v in local.neg_zonal : [
       for vv in v.endpoints : merge(vv, { neg = k, zone = v.zone })
     ]
   ])
@@ -31,6 +31,19 @@ locals {
   neg_endpoints = {
     for v in local._neg_endpoints :
     "${v.neg}-${v.ip_address}-${coalesce(v.port, "none")}" => v
+  }
+  neg_regional = {
+    for k, v in var.neg_configs : k => v.cloudrun if v.cloudrun != null
+  }
+  neg_zonal = {
+    # we need to rebuild new objects as we cannot merge different types
+    for k, v in var.neg_configs : k => {
+      endpoints  = v.gce != null ? v.gce.endpoints : v.hybrid.endpoints
+      network    = v.gce != null ? v.gce.network : v.hybrid.network
+      subnetwork = v.gce != null ? v.gce.subnetwork : null
+      type       = v.gce != null ? "GCE_VM_IP_PORT" : "NON_GCP_PRIVATE_IP_PORT"
+      zone       = v.gce != null ? v.gce.zone : v.hybrid.zone
+    } if v.gce != null || v.hybrid != null
   }
   proxy_ssl_certificates = concat(
     coalesce(var.ssl_certificates.certificate_ids, []),
@@ -101,23 +114,21 @@ resource "google_compute_instance_group" "default" {
 }
 
 resource "google_compute_network_endpoint_group" "default" {
-  for_each = var.neg_configs
+  for_each = local.neg_zonal
   project  = var.project_id
   zone     = each.value.zone
   name     = "${var.name}-${each.key}"
   # re-enable once provider properly supports this
   # default_port = each.value.default_port
-  description = var.description
-  network_endpoint_type = (
-    each.value.is_hybrid
-    ? "NON_GCP_PRIVATE_IP_PORT"
-    : "GCE_VM_IP_PORT"
+  description           = var.description
+  network_endpoint_type = each.value.type
+  network = (
+    each.value.network != null ? each.value.network : var.vpc_config.network
   )
-  network = try(each.value.vpc_config.network, var.vpc_config.network)
   subnetwork = (
-    each.value.is_hybrid
+    each.value.type == "NON_GCP_PRIVATE_IP_PORT"
     ? null
-    : try(each.value.vpc_config.subnetwork, var.vpc_config.subnetwork)
+    : try(each.value.subnetwork, var.vpc_config.subnetwork)
   )
 }
 
@@ -127,9 +138,22 @@ resource "google_compute_network_endpoint" "default" {
   network_endpoint_group = (
     google_compute_network_endpoint_group.default[each.value.neg].name
   )
-  instance   = each.value.instance
+  instance   = try(each.value.instance, null)
   ip_address = each.value.ip_address
   port       = each.value.port
   zone       = each.value.zone
 }
 
+resource "google_compute_region_network_endpoint_group" "default" {
+  for_each              = local.neg_regional
+  project               = var.project_id
+  region                = each.value.region
+  name                  = "${var.name}-${each.key}"
+  description           = var.description
+  network_endpoint_type = "SERVERLESS"
+  cloud_run {
+    service  = try(each.value.target_service.name, null)
+    tag      = try(each.value.target_service.tag, null)
+    url_mask = each.value.target_urlmask
+  }
+}
