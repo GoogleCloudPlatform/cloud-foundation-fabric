679bb6c063ca9f0af037e606c850aeb1a4db0a06
Sebastian Kunze <mail@sebastiankunze.de>
2022-11-16 10:01:28 +0100

    Use dark Docker magic to make Squid log to /dev/stdout

--- modules/cloud-config-container/squid/docker/entrypoint.sh
+++ modules/cloud-config-container/squid/docker/entrypoint.sh
@@ -46,10 +46,10 @@ fi
 if [[ -z ${1} ]]; then
   if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
     echo "Initializing cache..."
-    $(which squid) -N -f /etc/squid/squid.conf -z
+    sudo $(which squid) -N -f /etc/squid/squid.conf -z
   fi
   echo "Starting squid..."
-  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  exec sudo $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
 else
   exec "$@"
 fi
