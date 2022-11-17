a287b43ffd5075b60f24161271e41cb5ac6bdecd
Sebastian Kunze <mail@sebastiankunze.de>
2022-11-17 10:08:27 +0100

    .

--- modules/cloud-config-container/instance.tf
+++ modules/cloud-config-container/instance.tf
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */ ##
 
 resource "google_service_account" "default" {
   count        = var.test_instance == null ? 0 : 1
