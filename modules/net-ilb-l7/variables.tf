002ba4eef71aa56fbf8906c0848aa4a73744a8bb
Ludovico Magnocavallo <ludomagno@google.com>
2022-11-18 09:48:41 +0100

    Allow cross-project backend services in ILB L7 module (#991)
    * wip
    
    * neg, README
    
    * fix neg variable
    
    * fix neg project id, enable global access, add service directory

--- modules/net-ilb-l7/variables.tf
+++ modules/net-ilb-l7/variables.tf
@@ -26,12 +26,20 @@ variable "description" {
   default     = "Terraform managed."
 }
 
+# during the preview phase you cannot change this attribute on an existing rule
+variable "global_access" {
+  description = "Allow client access from all regions."
+  type        = bool
+  default     = null
+}
+
 variable "group_configs" {
   description = "Optional unmanaged groups to create. Can be referenced in backends via key or outputs."
   type = map(object({
     zone        = string
     instances   = optional(list(string), [])
     named_ports = optional(map(number), {})
+    project_id  = optional(string)
   }))
   default  = {}
   nullable = false
@@ -51,6 +59,7 @@ variable "name" {
 variable "neg_configs" {
   description = "Optional network endpoint groups to create. Can be referenced in backends via key or outputs."
   type = map(object({
+    project_id = optional(string)
     cloudrun = optional(object({
       region = string
       target_service = optional(object({
@@ -141,6 +150,15 @@ variable "protocol" {
 variable "region" {
   description = "The region where to allocate the ILB resources."
   type        = string
+}
+
+variable "service_directory_registration" {
+  description = "Service directory namespace and service used to register this load balancer."
+  type = object({
+    namespace = string
+    service   = string
+  })
+  default = null
 }
 
 variable "ssl_certificates" {
