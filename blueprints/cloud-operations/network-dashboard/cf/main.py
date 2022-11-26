651a893839b2b892f175181675a8516b21e54a6a
Ludo <ludomagno@google.com>
2022-11-26 09:08:43 +0100

    start documenting code

--- blueprints/cloud-operations/network-dashboard/cf/main.py
+++ blueprints/cloud-operations/network-dashboard/cf/main.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+'Network dashboard: create network-related metric timeseries for GCP resources.'
 
 import collections
 import json
@@ -33,28 +34,43 @@ Result = collections.namedtuple('Result', 'phase resource data')
 
 
 def do_discovery(resources):
-  'Discover resources needed in measurements.'
+  '''Calls discovery plugin functions and collect discovered resources.
+
+  The communication with discovery plugins uses double dispatch, where plugins
+  accept either no args and return 1-n HTTP request instances, or a single HTTP
+  response and return 1-n resource instances. A queue is set up for each plugin
+  results since each call can return multiple requests or resources.
+
+  Args:
+    resources: pre-initialized map where discovered resources will be stored.
+  '''
   LOGGER.info(f'discovery start')
   for plugin in plugins.get_discovery_plugins():
+    # set up the queue with the initial list of HTTP requests from this plugin
     q = collections.deque(plugin.func(resources))
     while q:
       result = q.popleft()
       if isinstance(result, plugins.HTTPRequest):
+        # fetch a single HTTP request
         response = fetch(result)
         if not response:
           continue
         if result.json:
           try:
+            # decode the JSON HTTP response and pass it to the plugin
             results = plugin.func(resources, response, response.json())
           except json.decoder.JSONDecodeError as e:
             LOGGER.critical(
                 f'error decoding JSON for {result.url}: {e.args[0]}')
             continue
         else:
+          # pass the raw HTTP response to the plugin
           results = plugin.func(resources, response)
         q += collections.deque(results)
       elif isinstance(result, plugins.Resource):
+        # store a resource the plugin derived from a previous HTTP response
         if result.key:
+          # this specific resource is indexed by an additional key
           resources[result.type][result.id][result.key] = result.data
         else:
           resources[result.type][result.id] = result.data
@@ -64,7 +80,14 @@ def do_discovery(resources):
 
 def do_init(resources, organization, op_project, folders=None, projects=None,
             custom_quota=None):
-  'Prepare the resources datastructure fields.'
+  '''Calls init plugins to configure keys in the shared resource map.
+
+  Args:
+    organization: organization id from configuration.
+    op_project: monitoring project id id from configuration.
+    folders: list of folder ids for resource discovery from configuration.
+    projects: list of project ids for resource discovery from configuration.
+  '''
   LOGGER.info(f'init start')
   resources['config:organization'] = str(organization)
   resources['config:monitoring_project'] = op_project
@@ -77,7 +100,17 @@ def do_init(resources, organization, op_project, folders=None, projects=None,
 
 
 def do_timeseries_calc(resources, descriptors, timeseries, debug_plugin=None):
-  'Calculate descriptors and timeseries.'
+  '''Calls timeseries plugins and collect resulting descriptors and timeseries.
+
+  Timeseries plugin return a list of MetricDescriptors and Timeseries instances,
+  one per each metric.
+
+  Args:
+    resources: shared map of configuration and discovered resources.
+    descriptors: list where collected descriptors will be stored.
+    timeseries: list where collected timeseries will be stored.
+    debug_plugin: optional name of a single plugin to call
+  '''
   LOGGER.info(f'timeseries calc start (debug plugin: {debug_plugin})')
   for plugin in plugins.get_timeseries_plugins():
     if debug_plugin and plugin.name != debug_plugin:
@@ -87,6 +120,7 @@ def do_timeseries_calc(resources, descriptors, timeseries, debug_plugin=None):
     for result in plugin.func(resources):
       if not result:
         continue
+      # append result to the relevant collection (descriptors or timeseries)
       if isinstance(result, plugins.MetricDescriptor):
         descriptors.append(result)
         num_desc += 1
@@ -99,7 +133,13 @@ def do_timeseries_calc(resources, descriptors, timeseries, debug_plugin=None):
 
 
 def do_timeseries_descriptors(project_id, existing, computed):
-  'Post timeseries descriptors.'
+  '''Executes API calls for each previously computed metric descriptor.
+
+  Args:
+    project_id: monitoring project id where to write descriptors.
+    existing: map of existing descriptor types.
+    computed: list of plugins.MetricDescriptor instances previously computed.
+  '''
   LOGGER.info('timeseries descriptors start')
   requests = plugins.monitoring.descriptor_requests(project_id, MONITORING_ROOT,
                                                     existing, computed)
@@ -112,7 +152,13 @@ def do_timeseries_descriptors(project_id, existing, computed):
 
 
 def do_timeseries(project_id, timeseries, descriptors):
-  'Post timeseries.'
+  '''Executes API calls for each previously computed timeseries.
+
+  Args:
+    project_id: monitoring project id where to write timeseries.
+    timeseries: list of plugins.Timeseries instances.
+    descriptors: list of plugins.MetricDescriptor instances matching timeseries.
+  '''
   LOGGER.info('timeseries start')
   requests = plugins.monitoring.timeseries_requests(project_id, MONITORING_ROOT,
                                                     timeseries, descriptors)
@@ -125,7 +171,16 @@ def do_timeseries(project_id, timeseries, descriptors):
 
 
 def fetch(request):
-  'Minimal HTTP client interface for API calls.'
+  '''Minimal HTTP client interface for API calls.
+
+  Executes the HTTP request passed as argument using the google.auth
+  authenticated session.
+
+  Args:
+    request: an instance of plugins.HTTPRequest.
+  Returns:
+    JSON-decoded or raw response depending on the 'json' request attribute.
+  '''
   # try
   LOGGER.info(f'fetch {"POST" if request.data else "GET"} {request.url}')
   try:
@@ -142,7 +197,6 @@ def fetch(request):
     LOGGER.critical(response.content)
     print(request.data)
     raise SystemExit(1)
-    return
   return response
 
 
@@ -166,6 +220,7 @@ def fetch(request):
 def main(organization=None, op_project=None, project=None, folder=None,
          custom_quota_file=None, dump_file=None, load_file=None,
          debug_plugin=None):
+  'CLI entry point.'
   logging.basicConfig(level=logging.INFO)
   descriptors = []
   timeseries = []
