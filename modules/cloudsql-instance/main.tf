b7fa0b313856b2e90917f3cf282de6813a5e4df6
Prabha Arya <prabhaarya@google.com>
2022-12-12 15:12:31 +0000

    add ssl certs to cloudsql instance

--- modules/cloudsql-instance/main.tf
+++ modules/cloudsql-instance/main.tf
@@ -175,3 +175,10 @@ resource "google_sql_user" "users" {
   host     = each.value.host
   password = each.value.password
 }
+
+resource "google_sql_ssl_cert" "postgres_client_cert" {
+  for_each    = var.postgres_client_cert != null ? toset(var.postgres_client_cert) : toset([])
+  provider    = google-beta
+  instance    = google_sql_database_instance.primary.name
+  common_name = each.key
+}
