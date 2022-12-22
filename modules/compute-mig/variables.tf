cb5e5a16d34dc432c2924c3824a6ffd2213c92ca
Ludo <ludomagno@google.com>
2022-12-22 17:59:58 +0100

    fix MIG health check variable

--- modules/compute-mig/variables.tf
+++ modules/compute-mig/variables.tf
@@ -167,7 +167,7 @@ variable "health_check_config" {
       (try(var.health_check_config.http, null) == null ? 0 : 1) +
       (try(var.health_check_config.http2, null) == null ? 0 : 1) +
       (try(var.health_check_config.https, null) == null ? 0 : 1) +
-      (try(var.health_check_config.tcp, null) == null ? 0 : 1)
+      (try(var.health_check_config.tcp, null) == null ? 0 : 1) +
       (try(var.health_check_config.ssl, null) == null ? 0 : 1) <= 1
     )
     error_message = "Only one health check type can be configured at a time."
