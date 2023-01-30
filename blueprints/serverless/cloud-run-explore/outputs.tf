93276d03d85d3a2d0ae5bf55a02fb2ede500a86d
Julio Diez <jdiez@google.com>
2023-01-30 16:34:52 +0100

    Add custom domain output

--- blueprints/serverless/cloud-run-explore/outputs.tf
+++ blueprints/serverless/cloud-run-explore/outputs.tf
@@ -7,3 +7,10 @@ output "load_balancer_ip" {
   description = "LB IP that forwards to Cloud Run service"
   value       = var.glb_create ? module.glb[0].address : "none"
 }
+
+# Custom domain for the Load Balancer. I'd prefer getting the value from the
+# SSL certificate but it is not exported as output
+output "custom_domain" {
+  description = "Custom domain for the Load Balancer"
+  value       = var.glb_create ? var.custom_domain : "none"
+}
