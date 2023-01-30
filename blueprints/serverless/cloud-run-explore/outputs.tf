e10c6ed62f2260bd3b632a95d8521dced02ddd6c
Julio Diez <jdiez@google.com>
2023-01-30 14:35:20 +0100

    Output URL as single string

--- blueprints/serverless/cloud-run-explore/outputs.tf
+++ blueprints/serverless/cloud-run-explore/outputs.tf
@@ -1,4 +1,4 @@
 output "default_URL" {
   description = "Cloud Run service default URL"
-  value       = module.cloud_run.service.status[*].url
+  value       = module.cloud_run.service.status[0].url
 }
