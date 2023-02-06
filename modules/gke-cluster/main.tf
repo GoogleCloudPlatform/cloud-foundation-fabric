e8c0b66aee61e9591902f1e99fae79b725fc870a
Brent Walker <brent.walker@outdoorsy.co>
2023-02-06 10:39:13 -0500

    Remove unnecessary ternaries

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -145,10 +145,10 @@ resource "google_container_cluster" "cluster" {
       dynamic "auto_provisioning_defaults" {
         for_each = var.cluster_autoscaling.auto_provisioning_defaults != null ? [""] : []
         content {
-          boot_disk_kms_key = var.cluster_autoscaling.auto_provisioning_defaults.boot_disk_kms_key != null ? var.cluster_autoscaling.auto_provisioning_defaults.boot_disk_kms_key : null
-          image_type        = var.cluster_autoscaling.auto_provisioning_defaults.image_type != null ? var.cluster_autoscaling.auto_provisioning_defaults.image_type : null
-          oauth_scopes      = var.cluster_autoscaling.auto_provisioning_defaults.oauth_scopes != null ? var.cluster_autoscaling.auto_provisioning_defaults.oauth_scopes : null
-          service_account   = var.cluster_autoscaling.auto_provisioning_defaults.service_account != null ? var.cluster_autoscaling.auto_provisioning_defaults.service_account : null
+          boot_disk_kms_key = var.cluster_autoscaling.auto_provisioning_defaults.boot_disk_kms_key
+          image_type        = var.cluster_autoscaling.auto_provisioning_defaults.image_type
+          oauth_scopes      = var.cluster_autoscaling.auto_provisioning_defaults.oauth_scopes
+          service_account   = var.cluster_autoscaling.auto_provisioning_defaults.service_account
         }
       }
       dynamic "resource_limits" {
