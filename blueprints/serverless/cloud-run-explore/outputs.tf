a8de9088c6202d9575f28c1e73954ac236993221
Julio Diez <jdiez@google.com>
2023-02-10 12:46:34 +0100

    Alphabetical ordering for outputs and variables

--- blueprints/serverless/cloud-run-explore/outputs.tf
+++ blueprints/serverless/cloud-run-explore/outputs.tf
@@ -13,7 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
+# Custom domain for the Load Balancer. I'd prefer getting the value from the
+# SSL certificate but it is not exported as output
+output "custom_domain" {
+  description = "Custom domain for the Load Balancer"
+  value       = local.gclb_create ? var.custom_domain : "none"
+}
+
 output "default_URL" {
   description = "Cloud Run service default URL"
   value       = module.cloud_run.service.status[0].url
@@ -22,11 +29,4 @@ output "default_URL" {
 output "load_balancer_ip" {
   description = "LB IP that forwards to Cloud Run service"
   value       = local.gclb_create ? module.glb[0].address : "none"
-}
-
-# Custom domain for the Load Balancer. I'd prefer getting the value from the
-# SSL certificate but it is not exported as output
-output "custom_domain" {
-  description = "Custom domain for the Load Balancer"
-  value       = local.gclb_create ? var.custom_domain : "none"
 }
--- blueprints/serverless/cloud-run-explore/variables.tf
+++ blueprints/serverless/cloud-run-explore/variables.tf
@@ -13,7 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
+variable "custom_domain" {
+  description = "Custom domain for the Load Balancer"
+  type        = string
+  default     = null
+}
+
+variable "iap" {
+  description = "Identity-Aware Proxy for Cloud Run in the LB"
+  type = object({
+    enabled            = optional(bool, false)
+    app_title          = optional(string, "Cloud Run Explore Application")
+    oauth2_client_name = optional(string, "Test Client")
+    email              = optional(string)
+  })
+  default = {}
+}
+
+variable "image" {
+  description = "Container image to deploy"
+  type        = string
+  default     = "us-docker.pkg.dev/cloudrun/container/hello"
+}
+
+variable "ingress_settings" {
+  description = "Ingress traffic sources allowed to call the service"
+  type        = string
+  default     = "all"
+}
+
 variable "project_id" {
   description = "Project ID"
   type        = string
@@ -31,41 +60,12 @@ variable "run_svc_name" {
   default     = "hello"
 }
 
-variable "image" {
-  description = "Container image to deploy"
-  type        = string
-  default     = "us-docker.pkg.dev/cloudrun/container/hello"
-}
-
-variable "custom_domain" {
-  description = "Custom domain for the Load Balancer"
-  type        = string
-  default     = null
-}
-
-variable "ingress_settings" {
-  description = "Ingress traffic sources allowed to call the service"
-  type        = string
-  default     = "all"
-}
-
 variable "security_policy" {
   description = "Security policy (Cloud Armor) to enforce in the LB"
   type = object({
     enabled      = optional(bool, false)
     ip_blacklist = optional(list(string), ["*"])
     path_blocked = optional(string, "/login.html")
-  })
-  default = {}
-}
-
-variable "iap" {
-  description = "Identity-Aware Proxy for Cloud Run in the LB"
-  type = object({
-    enabled            = optional(bool, false)
-    app_title          = optional(string, "Cloud Run Explore Application")
-    oauth2_client_name = optional(string, "Test Client")
-    email              = optional(string)
   })
   default = {}
 }
