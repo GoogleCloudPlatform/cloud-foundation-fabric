1c302c7ab331afbe32b78655aea4a1b02d6e4efb
Aleksandr Averbukh <averbukh@google.com>
2023-02-18 21:48:05 +0100

    TF formatting

--- modules/projects-data-source/main.tf
+++ modules/projects-data-source/main.tf
@@ -15,25 +15,25 @@
  */
 
 locals {
-  _ignore_folder_numbers = [for folder_id in var.ignore_folders: trimprefix(folder_id, "folders/")]
-  _ignore_folders_query = join(" AND NOT folders:", concat([""], local._ignore_folder_numbers))
+  _ignore_folder_numbers = [for folder_id in var.ignore_folders : trimprefix(folder_id, "folders/")]
+  _ignore_folders_query  = join(" AND NOT folders:", concat([""], local._ignore_folder_numbers))
   query = var.query != "" ? (
     format("%s%s", var.query, local._ignore_folders_query)
-  ) : (
+    ) : (
     format("%s%s", var.query, trimprefix(local._ignore_folders_query, " AND "))
   )
 
-  ignore_patterns = [for item in var.ignore_projects: "^${replace(item, "*", ".*")}$"]
+  ignore_patterns = [for item in var.ignore_projects : "^${replace(item, "*", ".*")}$"]
   ignore_regexp   = length(local.ignore_patterns) > 0 ? join("|", local.ignore_patterns) : "^NO_PROJECTS_TO_IGNORE$"
-  projects_after_ignore = [ for item in data.google_cloud_asset_resources_search_all.projects.results : item if (
-      length(concat(try(regexall(local.ignore_regexp, trimprefix(item.project, "projects/")), []), try(regexall(local.ignore_regexp, trimprefix(item.name, "//cloudresourcemanager.googleapis.com/projects/")), []))) == 0
+  projects_after_ignore = [for item in data.google_cloud_asset_resources_search_all.projects.results : item if(
+    length(concat(try(regexall(local.ignore_regexp, trimprefix(item.project, "projects/")), []), try(regexall(local.ignore_regexp, trimprefix(item.name, "//cloudresourcemanager.googleapis.com/projects/")), []))) == 0
     ) || contains(var.include_projects, trimprefix(item.name, "//cloudresourcemanager.googleapis.com/projects/")) || contains(var.include_projects, trimprefix(item.project, "projects/"))
   ]
 }
 
-data google_cloud_asset_resources_search_all projects {
+data "google_cloud_asset_resources_search_all" "projects" {
   provider = google-beta
-  scope = var.parent
+  scope    = var.parent
   asset_types = [
     "cloudresourcemanager.googleapis.com/Project"
   ]
