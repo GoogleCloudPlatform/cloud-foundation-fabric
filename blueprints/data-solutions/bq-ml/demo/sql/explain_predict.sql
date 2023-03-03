0e535732861736073c425456c9607ded4a3b6426
Giorgio Conte <conteg@google.com>
2023-03-03 14:52:35 +0000

    sql linting

--- blueprints/data-solutions/bq-ml/demo/sql/explain_predict.sql
+++ blueprints/data-solutions/bq-ml/demo/sql/explain_predict.sql
@@ -14,11 +14,12 @@
 * limitations under the License. 
 */
 
-select *
-from ML.EXPLAIN_PREDICT(MODEL `{project-id}.{dataset}.{model-name}`,
-  (select * except (session_id, session_starting_ts, user_id, has_purchased) 
-  from `{project-id}.{dataset}.ecommerce_abt`
-  where extract(ISOYEAR from session_starting_ts) = 2023
+SELECT *
+FROM ML.EXPLAIN_PREDICT(MODEL `{project-id}.{dataset}.{model-name}`,
+  (SELECT * EXCEPT (session_id, session_starting_ts, user_id, has_purchased) 
+  FROM `{project-id}.{dataset}.ecommerce_abt`
+  WHERE extract(ISOYEAR FROM session_starting_ts) = 2023
   ),
-  STRUCT(5 AS top_k_features, 0.5 as threshold)
+  STRUCT(5 AS top_k_features, 0.5 AS threshold)
 )
+LIMIT 100
