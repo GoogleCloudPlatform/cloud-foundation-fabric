8fc9549c5803163fbe3c6cada0966c08bbf2bf84
Ludovico Magnocavallo <ludomagno@google.com>
2023-03-05 17:08:43 +0100

    add support for proxy and psc subnets to module factory (#1211)

--- fast/stages/2-networking-d-separate-envs/spoke-dev.tf
+++ fast/stages/2-networking-d-separate-envs/spoke-dev.tf
@@ -43,13 +43,12 @@ module "dev-spoke-project" {
 }
 
 module "dev-spoke-vpc" {
-  source             = "../../../modules/net-vpc"
-  project_id         = module.dev-spoke-project.project_id
-  name               = "dev-spoke-0"
-  mtu                = 1500
-  data_folder        = "${var.factories_config.data_dir}/subnets/dev"
-  psa_config         = try(var.psa_ranges.dev, null)
-  subnets_proxy_only = local.l7ilb_subnets.dev
+  source      = "../../../modules/net-vpc"
+  project_id  = module.dev-spoke-project.project_id
+  name        = "dev-spoke-0"
+  mtu         = 1500
+  data_folder = "${var.factories_config.data_dir}/subnets/dev"
+  psa_config  = try(var.psa_ranges.dev, null)
   # set explicit routes for googleapis in case the default route is deleted
   routes = {
     private-googleapis = {
