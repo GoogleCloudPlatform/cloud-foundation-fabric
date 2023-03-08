2dec503dc161c776cba808acd643644eda10f24a
bruzzechesse <bruzzechesse@google.com>
2023-03-08 23:33:18 +0100

    fix policy_based_routing.sh to deal with IF having either no LB or multiple LB associated

--- modules/cloud-config-container/simple-nva/files/policy_based_routing.sh
+++ modules/cloud-config-container/simple-nva/files/policy_based_routing.sh
@@ -15,15 +15,21 @@
 # limitations under the License.
 
 IF_NAME=$1
-IP_LB=$(ip r show table local | grep "$IF_NAME proto 66" | cut -f 2 -d " ")
-
-# Sleep while there's no load balancer IP route for this IF
-while [ -z $IP_LB ] ; do
-  sleep 2
-  IP_LB=$(ip r show table local | grep "$IF_NAME proto 66" | cut -f 2 -d " ")
-done
-
 IF_NUMBER=$(echo $IF_NAME | sed -e s/eth//)
+
+# Check whether there are LB for this network interface
+# Curl to forwarded-ips endpoint return indexes zero based (e.g. 0 \n 1) for every LB associated to the IF, "" otherwise
+IF_LB=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/$IF_NUMBER/forwarded-ips/ -H "Metadata-Flavor: Google" |  tr -s '\n' ' ')
+IP_LB=$(ip r show table local | grep "$IF_NAME proto 66" | cut -f 2 -d " " |  tr -s '\n' ' ')
+
+if [ ! -z "$IF_LB" ] ; then
+  # Sleep while there's no load balancer IP route for this IF
+  while [ -z "$IP_LB" ] ; do
+    sleep 2
+    IP_LB=$(ip r show table local | grep "$IF_NAME proto 66" | cut -f 2 -d " " |  tr -s '\n' ' ')
+  done
+fi
+
 IF_GW=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/$IF_NUMBER/gateway -H "Metadata-Flavor: Google")
 IF_IP=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/$IF_NUMBER/ip -H "Metadata-Flavor: Google")
 IF_NETMASK=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/$IF_NUMBER/subnetmask -H "Metadata-Flavor: Google")
@@ -31,4 +37,13 @@ IF_IP_PREFIX=$(/var/run/nva/ipprefix_by_netmask.sh $IF_NETMASK)
 grep -qxF "$((200 + $IF_NUMBER)) hc-$IF_NAME" /etc/iproute2/rt_tables || echo "$((200 + $IF_NUMBER)) hc-$IF_NAME" >>/etc/iproute2/rt_tables
 ip route add $IF_GW src $IF_IP dev $IF_NAME table hc-$IF_NAME
 ip route add default via $IF_GW dev $IF_NAME table hc-$IF_NAME
-ip rule add from $IP_LB/32 table hc-$IF_NAME
+
+# Skip route configuration when no LB is associated to the network interface
+if [ ! -z "$IF_LB" ] ; then
+  # Convert whitespace separated list of IPs to array
+  IPS_LB=($IP_LB)
+  for IP in "${IPS_LB[@]}" 
+  do
+    ip rule add from $IP/32 table hc-$IF_NAME
+  done
+fi
