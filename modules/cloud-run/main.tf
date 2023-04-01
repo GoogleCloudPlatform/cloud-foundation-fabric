92b71a5098bb49e2866b11bc2beec4f7c23becfd
Ludovico Magnocavallo <ludomagno@google.com>
2023-04-01 14:06:29 +0200

    Refactor cloud run module to use optionals and support all features (#1293)
    * refactor cloud run resource and variables
    
    * vpc connector variables and resource
    
    * refactor env variable, combine and fix env examples
    
    * fix volume example
    
    * fix traffic split example
    
    * fix eventarc/pubsub example
    
    * fix eventarc/audit log example
    
    * fix SA examples, fix examples formatting
    
    * refactor eventarc variable
    
    * tfdoc
    
    * add IAM to example
    
    * add examples for revision annotations and serverless connector
    
    * fix new examples
    
    * remove legacy module tests
    
    * blueprint tests
    
    * Add tests to cloud-run module
    
    ---------
    
    Co-authored-by: Julio Castillo <jccb@google.com>

--- modules/cloud-run/main.tf
+++ modules/cloud-run/main.tf
@@ -21,10 +21,12 @@ locals {
       "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.0.id
     }
     : (
-      try(var.revision_annotations.vpcaccess_connector, null) == null
+      var.revision_annotations.vpcaccess_connector == null
       ? {}
       : {
-        "run.googleapis.com/vpc-access-connector" = var.revision_annotations.vpcaccess_connector
+        "run.googleapis.com/vpc-access-connector" = (
+          var.revision_annotations.vpcaccess_connector
+        )
       }
     )
   )
@@ -35,20 +37,26 @@ locals {
   )
   prefix = var.prefix == null ? "" : "${var.prefix}-"
   revision_annotations = merge(
-    try(var.revision_annotations.autoscaling.max_scale, null) == null ? {} : {
-      "autoscaling.knative.dev/maxScale" = var.revision_annotations.autoscaling.max_scale
+    try(var.revision_annotations.autoscaling, null) == null ? {} : {
+      "autoscaling.knative.dev/maxScale" = (
+        var.revision_annotations.autoscaling.max_scale
+      )
     },
     try(var.revision_annotations.autoscaling.min_scale, null) == null ? {} : {
-      "autoscaling.knative.dev/minScale" = var.revision_annotations.autoscaling.min_scale
+      "autoscaling.knative.dev/minScale" = (
+        var.revision_annotations.autoscaling.min_scale
+      )
     },
-    try(var.revision_annotations.cloudsql_instances, null) == null ? {} : {
-      "run.googleapis.com/cloudsql-instances" = join(",", coalesce(
-        var.revision_annotations.cloudsql_instances, []
-      ))
+    length(var.revision_annotations.cloudsql_instances) == 0 ? {} : {
+      "run.googleapis.com/cloudsql-instances" = (
+        join(",", var.revision_annotations.cloudsql_instances)
+      )
     },
     local._vpcaccess_annotation,
-    try(var.revision_annotations.vpcaccess_egress, null) == null ? {} : {
-      "run.googleapis.com/vpc-access-egress" = var.revision_annotations.vpcaccess_egress
+    var.revision_annotations.vpcaccess_egress == null ? {} : {
+      "run.googleapis.com/vpc-access-egress" = (
+        var.revision_annotations.vpcaccess_egress
+      )
     },
   )
   revision_name = (
@@ -69,12 +77,21 @@ locals {
 }
 
 resource "google_vpc_access_connector" "connector" {
-  count         = local.vpc_connector_create ? 1 : 0
-  project       = var.project_id
-  name          = var.vpc_connector_create.name
-  region        = var.region
-  ip_cidr_range = var.vpc_connector_create.ip_cidr_range
-  network       = var.vpc_connector_create.vpc_self_link
+  count   = local.vpc_connector_create ? 1 : 0
+  project = var.project_id
+  name = (
+    var.vpc_connector_create.name != null
+    ? var.vpc_connector_create.name
+    : var.name
+  )
+  region         = var.region
+  ip_cidr_range  = var.vpc_connector_create.ip_cidr_range
+  network        = var.vpc_connector_create.vpc_self_link
+  machine_type   = var.vpc_connector_create.machine_type
+  max_instances  = var.vpc_connector_create.instances.max
+  max_throughput = var.vpc_connector_create.throughput.max
+  min_instances  = var.vpc_connector_create.instances.min
+  min_throughput = var.vpc_connector_create.throughput.min
 }
 
 resource "google_cloud_run_service" "service" {
@@ -85,54 +102,75 @@ resource "google_cloud_run_service" "service" {
 
   template {
     spec {
+      container_concurrency = var.container_concurrency
+      service_account_name  = local.service_account_email
+      timeout_seconds       = var.timeout_seconds
       dynamic "containers" {
-        for_each = var.containers == null ? {} : {
-          for i, container in var.containers : i => container
-        }
+        for_each = var.containers
         content {
           image   = containers.value.image
-          command = try(containers.value.options.command, null)
-          args    = try(containers.value.options.args, null)
+          args    = containers.value.args
+          command = containers.value.command
           dynamic "env" {
-            for_each = (
-              try(containers.value.options.env, null) == null
-              ? {}
-              : containers.value.options.env
-            )
+            for_each = containers.value.env
             content {
               name  = env.key
               value = env.value
             }
           }
           dynamic "env" {
-            for_each = (
-              try(containers.value.options.env_from, null) == null
-              ? {}
-              : containers.value.options.env_from
-            )
+            for_each = containers.value.env_from_key
             content {
               name = env.key
               value_from {
                 secret_key_ref {
-                  name = env.value.name
                   key  = env.value.key
+                  name = env.value.name
+                }
+              }
+            }
+          }
+          dynamic "liveness_probe" {
+            for_each = containers.value.liveness_probe == null ? [] : [""]
+            content {
+              failure_threshold     = containers.value.liveness_probe.failure_threshold
+              initial_delay_seconds = containers.value.liveness_probe.initial_delay_seconds
+              period_seconds        = containers.value.liveness_probe.period_seconds
+              timeout_seconds       = containers.value.liveness_probe.timeout_seconds
+              dynamic "grpc" {
+                for_each = (
+                  containers.value.liveness_probe.action.grpc == null ? [] : [""]
+                )
+                content {
+                  port    = containers.value.liveness_probe.action.grpc.port
+                  service = containers.value.liveness_probe.action.grpc.service
+                }
+              }
+              dynamic "http_get" {
+                for_each = (
+                  containers.value.liveness_probe.action.http_get == null ? [] : [""]
+                )
+                content {
+                  path = containers.value.liveness_probe.action.http_get.path
+                  dynamic "http_headers" {
+                    for_each = (
+                      containers.value.liveness_probe.action.http_get.http_headers
+                    )
+                    content {
+                      name  = http_headers.key
+                      value = http_headers.value
+                    }
+                  }
                 }
               }
             }
           }
           dynamic "ports" {
-            for_each = (
-              containers.value.ports == null
-              ? {}
-              : {
-                for port in containers.value.ports :
-                "${port.name}-${port.container_port}" => port
-              }
-            )
+            for_each = containers.value.ports
             content {
+              container_port = ports.value.container_port
               name           = ports.value.name
               protocol       = ports.value.protocol
-              container_port = ports.value.container_port
             }
           }
           dynamic "resources" {
@@ -142,12 +180,51 @@ resource "google_cloud_run_service" "service" {
               requests = containers.value.resources.requests
             }
           }
+          dynamic "startup_probe" {
+            for_each = containers.value.startup_probe == null ? [] : [""]
+            content {
+              failure_threshold     = containers.value.startup_probe.failure_threshold
+              initial_delay_seconds = containers.value.startup_probe.initial_delay_seconds
+              period_seconds        = containers.value.startup_probe.period_seconds
+              timeout_seconds       = containers.value.startup_probe.timeout_seconds
+              dynamic "grpc" {
+                for_each = (
+                  containers.value.startup_probe.action.grpc == null ? [] : [""]
+                )
+                content {
+                  port    = containers.value.startup_probe.action.grpc.port
+                  service = containers.value.startup_probe.action.grpc.service
+                }
+              }
+              dynamic "http_get" {
+                for_each = (
+                  containers.value.startup_probe.action.http_get == null ? [] : [""]
+                )
+                content {
+                  path = containers.value.startup_probe.action.http_get.path
+                  dynamic "http_headers" {
+                    for_each = (
+                      containers.value.startup_probe.action.http_get.http_headers
+                    )
+                    content {
+                      name  = http_headers.key
+                      value = http_headers.value
+                    }
+                  }
+                }
+              }
+              dynamic "tcp_socket" {
+                for_each = (
+                  containers.value.startup_probe.action.tcp_socket == null ? [] : [""]
+                )
+                content {
+                  port = containers.value.startup_probe.action.tcp_socket.port
+                }
+              }
+            }
+          }
           dynamic "volume_mounts" {
-            for_each = (
-              containers.value.volume_mounts == null
-              ? {}
-              : containers.value.volume_mounts
-            )
+            for_each = containers.value.volume_mounts
             content {
               name       = volume_mounts.key
               mount_path = volume_mounts.value
@@ -155,20 +232,19 @@ resource "google_cloud_run_service" "service" {
           }
         }
       }
-      service_account_name = local.service_account_email
       dynamic "volumes" {
-        for_each = var.volumes == null ? [] : var.volumes
+        for_each = var.volumes
         content {
-          name = volumes.value.name
+          name = volumes.key
           secret {
-            secret_name = volumes.value.secret_name
+            secret_name  = volumes.value.secret_name
+            default_mode = volumes.value.default_mode
             dynamic "items" {
-              for_each = (
-                volumes.value.items == null ? [] : volumes.value.items
-              )
+              for_each = volumes.value.items
               content {
-                key  = items.value.key
+                key  = items.key
                 path = items.value.path
+                mode = items.value.mode
               }
             }
           }
@@ -186,10 +262,16 @@ resource "google_cloud_run_service" "service" {
   }
 
   dynamic "traffic" {
-    for_each = var.traffic == null ? {} : var.traffic
+    for_each = var.traffic
     content {
-      percent       = traffic.value
-      revision_name = "${var.name}-${traffic.key}"
+      percent         = traffic.value.percent
+      latest_revision = traffic.value.latest == true
+      revision_name = (
+        traffic.value.latest == true
+        ? null
+        : "${var.name}-${traffic.key}"
+      )
+      tag = traffic.value.tag
     }
   }
 
@@ -217,11 +299,8 @@ resource "google_service_account" "service_account" {
 }
 
 resource "google_eventarc_trigger" "audit_log_triggers" {
-  for_each = var.audit_log_triggers == null ? {} : {
-    for trigger in var.audit_log_triggers :
-    "${trigger.service_name}-${trigger.method_name}" => trigger
-  }
-  name     = "${local.prefix}${each.key}-audit-log-trigger"
+  for_each = var.eventarc_triggers.audit_log
+  name     = "${local.prefix}audit-log-${each.key}"
   location = google_cloud_run_service.service.location
   project  = google_cloud_run_service.service.project
   matching_criteria {
@@ -230,11 +309,11 @@ resource "google_eventarc_trigger" "audit_log_triggers" {
   }
   matching_criteria {
     attribute = "serviceName"
-    value     = each.value.service_name
+    value     = each.value.service
   }
   matching_criteria {
     attribute = "methodName"
-    value     = each.value.method_name
+    value     = each.value.method
   }
   destination {
     cloud_run_service {
@@ -245,24 +324,17 @@ resource "google_eventarc_trigger" "audit_log_triggers" {
 }
 
 resource "google_eventarc_trigger" "pubsub_triggers" {
-  for_each = var.pubsub_triggers == null ? [] : toset(var.pubsub_triggers)
-  name = (
-    each.value == ""
-    ? "${local.prefix}default-pubsub-trigger"
-    : "${local.prefix}${each.value}-pubsub-trigger"
-  )
+  for_each = var.eventarc_triggers.pubsub
+  name     = "${local.prefix}pubsub-${each.key}"
   location = google_cloud_run_service.service.location
   project  = google_cloud_run_service.service.project
   matching_criteria {
     attribute = "type"
     value     = "google.cloud.pubsub.topic.v1.messagePublished"
   }
-  dynamic "transport" {
-    for_each = each.value == null ? [] : [""]
-    content {
-      pubsub {
-        topic = each.value
-      }
+  transport {
+    pubsub {
+      topic = each.value
     }
   }
   destination {
