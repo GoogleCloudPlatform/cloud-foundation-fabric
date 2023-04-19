da222ee79725315480c5f83515d9993e1cb22e22
bjohnrl <bjohn@rocketlawyer.co.uk>
2023-04-19 09:24:35 +0100

    Add lifecycle for create certificate before destroy

--- modules/net-ilb-l7/main.tf
+++ modules/net-ilb-l7/main.tf
@@ -88,7 +88,7 @@ resource "google_compute_region_ssl_certificate" "default" {
   name        = "${var.name}-${each.key}"
   certificate = each.value.certificate
   private_key = each.value.private_key
-  
+
   lifecycle {
     create_before_destroy = true
   }
