f1d16363e41ab3506932a2a86c83ddc6ed377865
Benoît Sauvère <benoit.sauvere@gmail.com>
2023-05-24 11:56:24 +0200

    allow to configure `stack_type` in the GKE modules

--- modules/gke-cluster-standard/main.tf
+++ modules/gke-cluster-standard/main.tf
@@ -162,6 +162,7 @@ resource "google_container_cluster" "cluster" {
     content {
       cluster_ipv4_cidr_block  = var.vpc_config.secondary_range_blocks.pods
       services_ipv4_cidr_block = var.vpc_config.secondary_range_blocks.services
+      stack_type               = try(var.vpc_config.stack_type, null)
     }
   }
   dynamic "ip_allocation_policy" {
@@ -169,6 +170,7 @@ resource "google_container_cluster" "cluster" {
     content {
       cluster_secondary_range_name  = var.vpc_config.secondary_range_names.pods
       services_secondary_range_name = var.vpc_config.secondary_range_names.services
+      stack_type                    = try(var.vpc_config.stack_type, null)
     }
   }
 
