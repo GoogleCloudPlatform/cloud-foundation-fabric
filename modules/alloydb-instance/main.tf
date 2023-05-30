e3c029e8083f69d8315ee967db7c22155b29c686
Prabha Arya <prabhaarya@google.com>
2023-05-30 15:16:08 +0100

    fix typos

--- modules/alloydb-instance/main.tf
+++ modules/alloydb-instance/main.tf
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 locals {
   read_pool_instance = (
     var.read_pool_instance != null ?
