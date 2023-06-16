0f349b9a8ce6fc45b93356901ba321404eece61b
lcaggio <lorenzo.caggioni@gmail.com>
2023-06-16 00:45:08 -0400

    Remove file.

--- blueprints/data-solutions/data-platform-minimal/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  impersonate_service_account = "terraform@lcaggioni-sandbox.iam.gserviceaccount.com"
-}
-provider "google-beta" {
-  impersonate_service_account = "terraform@lcaggioni-sandbox.iam.gserviceaccount.com"
-}
