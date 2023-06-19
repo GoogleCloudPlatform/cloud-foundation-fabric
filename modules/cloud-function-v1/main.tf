bfb6f58b179647447a96cc9c921f465fba3d4323
Ludo <ludomagno@google.com>
2023-06-19 11:52:23 +0200

    split v1

--- modules/cloud-function-v1/main.tf
+++ modules/cloud-function-v1/main.tf
@@ -24,17 +24,11 @@ locals {
       : null
     )
   )
-  function = (
-    var.v2
-    ? google_cloudfunctions2_function.function[0]
-    : google_cloudfunctions_function.function[0]
-  )
-  prefix                = var.prefix == null ? "" : "${var.prefix}-"
-  service_account_email = var.service_account_create ? google_service_account.service_account[0].email : var.service_account
-  trigger_service_account_email = (
-    coalesce(try(var.trigger_config.v2.service_account_create, false), false)
-    ? google_service_account.trigger_service_account[0].email
-    : null
+  prefix = var.prefix == null ? "" : "${var.prefix}-"
+  service_account_email = (
+    var.service_account_create
+    ? google_service_account.service_account[0].email
+    : var.service_account
   )
   vpc_connector = (
     var.vpc_connector == null
@@ -57,7 +51,6 @@ resource "google_vpc_access_connector" "connector" {
 }
 
 resource "google_cloudfunctions_function" "function" {
-  count                 = var.v2 ? 0 : 1
   project               = var.project_id
   region                = var.region
   name                  = "${local.prefix}${var.name}"
@@ -72,7 +65,7 @@ resource "google_cloudfunctions_function" "function" {
   source_archive_bucket = local.bucket
   source_archive_object = google_storage_bucket_object.bundle.name
   labels                = var.labels
-  trigger_http          = var.trigger_config.v1 == null ? true : null
+  trigger_http          = var.trigger_config == null ? true : null
 
   ingress_settings  = var.ingress_settings
   build_worker_pool = var.build_worker_pool
@@ -83,14 +76,14 @@ resource "google_cloudfunctions_function" "function" {
   )
 
   dynamic "event_trigger" {
-    for_each = var.trigger_config.v1 == null ? [] : [""]
+    for_each = var.trigger_config == null ? [] : [""]
     content {
-      event_type = var.trigger_config.v1.event
-      resource   = var.trigger_config.v1.resource
+      event_type = var.trigger_config.event
+      resource   = var.trigger_config.resource
       dynamic "failure_policy" {
-        for_each = var.trigger_config.v1.retry == null ? [] : [""]
+        for_each = var.trigger_config.retry == null ? [] : [""]
         content {
-          retry = var.trigger_config.v1.retry
+          retry = var.trigger_config.retry
         }
       }
     }
@@ -126,104 +119,11 @@ resource "google_cloudfunctions_function" "function" {
   }
 }
 
-resource "google_cloudfunctions2_function" "function" {
-  count       = var.v2 ? 1 : 0
-  provider    = google-beta
-  project     = var.project_id
-  location    = var.region
-  name        = "${local.prefix}${var.name}"
-  description = var.description
-  build_config {
-    worker_pool           = var.build_worker_pool
-    runtime               = var.function_config.runtime
-    entry_point           = "${var.function_config.entry_point}_http" # Set the entry point 
-    environment_variables = var.environment_variables
-    source {
-      storage_source {
-        bucket = local.bucket
-        object = google_storage_bucket_object.bundle.name
-      }
-    }
-  }
-  dynamic "event_trigger" {
-    for_each = var.trigger_config.v2 == null ? [] : [""]
-    content {
-      trigger_region = var.trigger_config.v2.region
-      event_type     = var.trigger_config.v2.event_type
-      pubsub_topic   = var.trigger_config.v2.pubsub_topic
-      dynamic "event_filters" {
-        for_each = var.trigger_config.v2.event_filters == null ? [] : var.trigger_config.v2.event_filters
-        iterator = event_filter
-        content {
-          attribute = event_filter.attribute
-          value     = event_filter.value
-          operator  = event_filter.operator
-        }
-      }
-      service_account_email = var.trigger_config.v2.service_account_email
-      retry_policy          = var.trigger_config.v2.retry_policy
-    }
-  }
-  service_config {
-    max_instance_count             = var.function_config.instance_count
-    min_instance_count             = 0
-    available_memory               = "${var.function_config.memory_mb}M"
-    available_cpu                  = var.function_config.cpu
-    timeout_seconds                = var.function_config.timeout_seconds
-    environment_variables          = var.environment_variables
-    ingress_settings               = var.ingress_settings
-    all_traffic_on_latest_revision = true
-    service_account_email          = local.service_account_email
-    vpc_connector                  = local.vpc_connector
-    vpc_connector_egress_settings = try(
-    var.vpc_connector.egress_settings, null)
-
-    dynamic "secret_environment_variables" {
-      for_each = { for k, v in var.secrets : k => v if !v.is_volume }
-      iterator = secret
-      content {
-        key        = secret.key
-        project_id = secret.value.project_id
-        secret     = secret.value.secret
-        version    = try(secret.value.versions.0, "latest")
-      }
-    }
-
-    dynamic "secret_volumes" {
-      for_each = { for k, v in var.secrets : k => v if v.is_volume }
-      iterator = secret
-      content {
-        mount_path = secret.key
-        project_id = secret.value.project_id
-        secret     = secret.value.secret
-        dynamic "versions" {
-          for_each = secret.value.versions
-          iterator = version
-          content {
-            path    = split(":", version)[1]
-            version = split(":", version)[0]
-          }
-        }
-      }
-    }
-  }
-  labels = var.labels
-}
-
 resource "google_cloudfunctions_function_iam_binding" "default" {
-  for_each       = !var.v2 ? var.iam : {}
+  for_each       = var.iam
   project        = var.project_id
   region         = var.region
-  cloud_function = local.function.name
-  role           = each.key
-  members        = each.value
-}
-
-resource "google_cloudfunctions2_function_iam_binding" "default" {
-  for_each       = var.v2 ? var.iam : {}
-  project        = var.project_id
-  location       = google_cloudfunctions2_function.function[0].location
-  cloud_function = local.function.name
+  cloud_function = google_cloudfunctions_function.function.id
   role           = each.key
   members        = each.value
 }
@@ -278,18 +178,4 @@ resource "google_service_account" "service_account" {
   project      = var.project_id
   account_id   = "tf-cf-${var.name}"
   display_name = "Terraform Cloud Function ${var.name}."
-}
-
-resource "google_service_account" "trigger_service_account" {
-  count        = coalesce(try(var.trigger_config.v2.service_account_create, false), false) ? 1 : 0
-  project      = var.project_id
-  account_id   = "tf-cf-trigger-${var.name}"
-  display_name = "Terraform trigger for Cloud Function ${var.name}."
-}
-
-resource "google_project_iam_member" "trigger_iam" {
-  count   = coalesce(try(var.trigger_config.v2.service_account_create, false), false) ? 1 : 0
-  project = var.project_id
-  member  = "serviceAccount:${google_service_account.trigger_service_account[0].email}"
-  role    = "roles/run.invoker"
 }
