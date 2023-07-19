3d6df58611b62385b1d73121da499aa3139c85a7
Keith Harvey <keith.harvey@timeout.com>
2023-07-19 14:42:36 +0100

    latest

--- blueprints/data-solutions/data-platform-minimal/01-ga-landing.tf
+++ blueprints/data-solutions/data-platform-minimal/01-ga-landing.tf
@@ -62,7 +62,7 @@ module "ga-land-project" {
 module "ga-land-sa-0" {
   count          = var.enable_services.ga_project == true ? 1 : 0
   source       = "../../../modules/iam-service-account"
-  project_id   = optional(module.ga-land-project.project_id)
+  project_id   = optional(string, module.ga-land-project.project_id)
   prefix       = var.prefix
   name         = "ga-lnd-sa-0"
   display_name = "Data platform GA landing zone service account."
