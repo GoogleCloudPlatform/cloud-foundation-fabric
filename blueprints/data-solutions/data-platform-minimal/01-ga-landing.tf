5839d3fbcb4c377a9f8481b71d7bf6eb565d6fb0
Keith Harvey <keith.harvey@timeout.com>
2023-07-20 11:08:09 +0100

    latest

--- blueprints/data-solutions/data-platform-minimal/01-ga-landing.tf
+++ blueprints/data-solutions/data-platform-minimal/01-ga-landing.tf
@@ -19,7 +19,6 @@ locals {
     "roles/storage.objectViewer"  = [module.processing-sa-cmp-0.iam_email]
     "roles/storage.objectAdmin"   = [module.processing-sa-0.iam_email]
   }
-  project_id = var.google_analytics_property_id != null ? module.ga-land-project.project_id : null
 }
 
 module "ga-land-project" {
@@ -62,7 +61,7 @@ module "ga-land-project" {
 module "ga-land-sa-0" {
   count   = var.google_analytics_property_id != null ? 1 : 0
   source       = "../../../modules/iam-service-account"
-  project_id   = local.project_id
+  project_id   = var.google_analytics_property_id != null ? module.ga-land-project.project_id : null
   prefix       = var.prefix
   name         = "ga-lnd-sa-0"
   display_name = "Data platform GA landing zone service account."
@@ -76,7 +75,7 @@ module "ga-land-sa-0" {
 module "ga-land-bq-0" {
   count   = var.google_analytics_property_id != null ? 1 : 0
   source         = "../../../modules/bigquery-dataset"
-  project_id     = local.project_id
+  project_id     = var.google_analytics_property_id != null ? module.ga-land-project.project_id : null
   id             = var.google_analytics_property_id
   location       = var.location
   encryption_key = var.service_encryption_keys.bq
