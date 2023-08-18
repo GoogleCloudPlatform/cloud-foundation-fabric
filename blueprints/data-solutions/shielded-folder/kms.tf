983581b3d1a29b2711a562935bfbd8d4ae48305c
Ludo <ludomagno@google.com>
2023-08-18 09:33:58 +0200

    shielded folder fix

--- blueprints/data-solutions/shielded-folder/kms.tf
+++ blueprints/data-solutions/shielded-folder/kms.tf
@@ -25,12 +25,9 @@ locals {
       for k, v in var.kms_keys : k => v if contains(v.locations, loc)
     }
   }
-
   kms_log_locations = distinct(flatten([
     for k, v in local.kms_log_sink_keys : compact(v.locations)
   ]))
-
-  # Log sink keys
   kms_log_sink_keys = {
     "storage" = {
       labels          = {}
@@ -61,8 +58,12 @@ module "sec-project" {
   name            = var.project_config.project_ids["sec-core"]
   parent          = module.folder.id
   billing_account = var.project_config.billing_account_id
-  project_create  = var.project_config.billing_account_id != null && var.enable_features.encryption
-  prefix          = var.project_config.billing_account_id == null ? null : var.prefix
+  project_create = (
+    var.project_config.billing_account_id != null && var.enable_features.encryption
+  )
+  prefix = (
+    var.project_config.billing_account_id == null ? null : var.prefix
+  )
   group_iam = {
     (local.groups.workload-security) = [
       "roles/editor"
@@ -76,16 +77,22 @@ module "sec-project" {
 }
 
 module "sec-kms" {
-  for_each   = var.enable_features.encryption ? toset(local.kms_locations) : toset([])
+  for_each = (
+    var.enable_features.encryption
+    ? toset(local.kms_locations)
+    : toset([])
+  )
   source     = "../../../modules/kms"
   project_id = module.sec-project[0].project_id
   keyring = {
     location = each.key
     name     = "sec-${each.key}"
   }
-  # rename to `key_iam` to switch to authoritative bindings
-  key_iam_additive = {
+  key_iam = {
     for k, v in local.kms_locations_keys[each.key] : k => v.iam
+  }
+  key_iam_bindings_additive = {
+    for k, v in local.kms_locations_keys[each.key] : k => v.iam_bindings_additive
   }
   keys = local.kms_locations_keys[each.key]
 }
