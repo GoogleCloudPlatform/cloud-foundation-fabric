c63884d52e8e8450d83e3c6cbe2efa861fe77775
Luca Prete <preteluca@gmail.com>
2023-08-28 17:32:30 +0200

    Remove unused ASN numbers in CloudNAT to avoid FAST provider errors

--- fast/stages/2-networking-e-nva-bgp/landing.tf
+++ fast/stages/2-networking-e-nva-bgp/landing.tf
@@ -86,7 +86,6 @@ module "landing-nat-primary" {
   router_create  = true
   router_name    = "prod-nat-${local.region_shortnames[var.regions.primary]}"
   router_network = module.landing-untrusted-vpc.name
-  router_asn     = 4200001024
 }
 
 moved {
@@ -102,7 +101,6 @@ module "landing-nat-secondary" {
   router_create  = true
   router_name    = "prod-nat-${local.region_shortnames[var.regions.secondary]}"
   router_network = module.landing-untrusted-vpc.name
-  router_asn     = 4200001024
 }
 
 # Trusted VPC
