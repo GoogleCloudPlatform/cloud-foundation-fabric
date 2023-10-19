1b145999811ecf7a50d346678683ebca1141b012
Wiktor Niesiobędzki <wiktorn@google.com>
2023-10-19 18:06:20 +0000

    Move to use provider resources directly

--- tests/examples_e2e/setup_module/main.tf
+++ tests/examples_e2e/setup_module/main.tf
@@ -12,77 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: shall we use our own modules for testing?
-
 locals {
   prefix = "${var.prefix}-${var.timestamp}-${var.suffix}"
-}
-
-module "folder" {
-  source = "../../../modules/folder"
-  parent = var.parent
-  name   = "E2E Tests ${var.timestamp}-${var.suffix}"
-}
-
-
-module "project" {
-  source          = "../../../modules/project"
-  billing_account = var.billing_account
-  name            = "prj"
-  parent          = module.folder.id
-  prefix          = local.prefix
-  services        = local.services
-}
-
-module "bucket" {
-  source        = "../../../modules/gcs"
-  name          = "bucket"
-  prefix        = local.prefix
-  project_id    = module.project.id
-  force_destroy = true
-}
-
-
-module "vpc" {
-  source     = "../../../modules/net-vpc"
-  name       = "e2e-test"
-  project_id = module.project.id
-  subnets = [
-    {
-      ip_cidr_range = "10.0.16.0/24"
-      name          = "e2e-test-1"
-      region        = var.region
-    }
-  ]
-}
-
-
-resource "local_file" "terraform_tfvars" {
-  filename = "e2e_tests.tfvars"
-  content = templatefile("e2e_tests.tfvars.tftpl", {
-    bucket             = module.bucket.name
-    billing_account_id = var.billing_account
-    organization_id    = var.organization_id
-    folder_id          = module.folder.id
-    prefix             = local.prefix
-    project_id         = module.project.id
-    region             = var.region
-    service_account = {
-      id        = "{service_account.id}"
-      email     = "{service_account.email}"
-      iam_email = "{service_account.iam_email}"
-    }
-    subnet = module.vpc.subnets["${var.region}/e2e-test-1"]
-    vpc    = module.vpc
-  })
-}
-
-locals {
   services = [
     # trimmed down list of services, to be extended as needed
     "cloudbuild.googleapis.com",
     "cloudfunctions.googleapis.com",
     "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
     "iam.googleapis.com",
     "run.googleapis.com",
     "serviceusage.googleapis.com",
@@ -90,4 +27,80 @@ locals {
     "storage-component.googleapis.com",
     "storage.googleapis.com",
   ]
+}
+
+resource "google_folder" "folder" {
+  display_name = "E2E Tests ${var.timestamp}-${var.suffix}"
+  parent       = var.parent
+}
+
+resource "google_project" "project" {
+  name            = "${local.prefix}-prj"
+  billing_account = var.billing_account
+  folder_id       = google_folder.folder.id
+  project_id      = "${local.prefix}-prj"
+}
+
+resource "google_project_service" "project_service" {
+  for_each = toset(local.services)
+  service  = each.value
+  project  = google_project.project.project_id
+}
+
+resource "google_storage_bucket" "bucket" {
+  location      = var.region
+  name          = "${local.prefix}-bucket"
+  project       = google_project.project.project_id
+  force_destroy = true
+  depends_on    = [google_project_service.project_service]
+}
+
+resource "google_compute_network" "network" {
+  name                    = "e2e-test"
+  project                 = google_project.project.project_id
+  auto_create_subnetworks = false
+  depends_on              = [google_project_service.project_service]
+}
+
+resource "google_compute_subnetwork" "subnetwork" {
+  ip_cidr_range = "10.0.16.0/24"
+  name          = "e2e-test-1"
+  network       = google_compute_network.network.name
+  project       = google_project.project.project_id
+  region        = var.region
+}
+
+resource "google_service_account" "service_account" {
+  account_id = "e2e-service-account"
+  project    = google_project.project.project_id
+  depends_on = [google_project_service.project_service]
+}
+
+resource "local_file" "terraform_tfvars" {
+  filename = "e2e_tests.tfvars"
+  content = templatefile("e2e_tests.tfvars.tftpl", {
+    bucket             = google_storage_bucket.bucket.name
+    billing_account_id = var.billing_account
+    organization_id    = var.organization_id
+    folder_id          = google_folder.folder.folder_id
+    prefix             = local.prefix
+    project_id         = google_project.project.project_id
+    region             = var.region
+    service_account = {
+      id        = google_service_account.service_account.id
+      email     = google_service_account.service_account.email
+      iam_email = "serviceAccount:${google_service_account.service_account.email}"
+    }
+    subnet = {
+      name          = google_compute_subnetwork.subnetwork.name
+      region        = google_compute_subnetwork.subnetwork.region
+      ip_cidr_range = google_compute_subnetwork.subnetwork.ip_cidr_range
+      self_link     = google_compute_subnetwork.subnetwork.self_link
+    }
+    vpc = {
+      name      = google_compute_network.network.name
+      self_link = google_compute_network.network.self_link
+      id        = google_compute_network.network.id
+    }
+  })
 }
