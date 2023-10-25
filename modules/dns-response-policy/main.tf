8360945a3f648cca2740a72d1b0304a061b224b5
Simone Ruffilli <ilruffo@gmail.com>
2023-10-25 13:45:36 +0200

    Fix dns-response-policy

--- modules/dns-response-policy/main.tf
+++ modules/dns-response-policy/main.tf
@@ -15,7 +15,7 @@
  */
 
 locals {
-  _factory_rules = yamldecode(file(var.rules_file))
+  _factory_rules = var.rules_file != null ? yamldecode(file(var.rules_file)) : {}
   factory_rules = {
     for k, v in local._factory_rules : k => {
       dns_name = v.dns_name
