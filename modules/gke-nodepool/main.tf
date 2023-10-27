98dde0c57f22aa08bc269582fcf6ae2ac78ff0c7
Ewa Wojtach <ewojtach@google.com>
2023-10-27 08:38:37 +0200

    review comments

--- modules/gke-nodepool/main.tf
+++ modules/gke-nodepool/main.tf
@@ -168,8 +168,10 @@ resource "google_container_node_pool" "nodepool" {
         gpu_partition_size = var.node_config.guest_accelerator.gpu_driver == null ? null : var.node_config.guest_accelerator.gpu_driver.partition_size
 
         dynamic "gpu_sharing_config" {
-          #for_each = var.node_config.guest_accelerator.gpu_driver != null ? [""] : []
-          for_each = var.node_config.guest_accelerator.gpu_driver == null ? [] : (var.node_config.guest_accelerator.gpu_driver.max_shared_clients_per_gpu != null ? [""] : [])
+          for_each = lookup(
+            lookup(var.node_config.guest_accelerator, "gpu_driver", {}),
+            "max_shared_clients_per_gpu"
+          ) != null ? [""] : []
           content {
             gpu_sharing_strategy       = var.node_config.guest_accelerator.gpu_driver.max_shared_clients_per_gpu != null ? "TIME_SHARING" : null
             max_shared_clients_per_gpu = var.node_config.guest_accelerator.gpu_driver.max_shared_clients_per_gpu
