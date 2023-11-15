5f2bc90bba791aa122b5648b3eb5361b7b2ffbbf
aurelien.legrand01@gmail.com <aurelien.legrand01@gmail.com>
2023-11-15 17:47:02 +0100

    python formatting

--- blueprints/cloud-operations/network-dashboard/src/main.py
+++ blueprints/cloud-operations/network-dashboard/src/main.py
@@ -81,8 +81,7 @@ def do_discovery(resources):
         else:
           resources[result.type][result.id] = result.data
   LOGGER.info('discovery end {}'.format(
-      {k: len(v) for k, v in resources.items() if not isinstance(v, str)
-  }))
+      {k: len(v) for k, v in resources.items() if not isinstance(v, str)}))
 
 
 def do_init(resources, discovery_root, monitoring_project, folders=None,
