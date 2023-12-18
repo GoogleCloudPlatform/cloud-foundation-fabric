88b91cdaeefab895e0117dbbd82fd7b6976494ef
Julio Diez <jdiez@google.com>
2023-12-18 11:53:28 +0100

    Move VPC connector management to specific files

--- modules/cloud-run-v2/main.tf
+++ modules/cloud-run-v2/main.tf
@@ -41,28 +41,6 @@ locals {
   )
 }
 
-resource "google_vpc_access_connector" "connector" {
-  count   = var.vpc_connector_create != null ? 1 : 0
-  project = var.project_id
-  name = (
-    var.vpc_connector_create.name != null
-    ? var.vpc_connector_create.name
-    : var.name
-  )
-  region         = var.region
-  ip_cidr_range  = var.vpc_connector_create.ip_cidr_range
-  network        = var.vpc_connector_create.vpc_self_link
-  machine_type   = var.vpc_connector_create.machine_type
-  max_instances  = var.vpc_connector_create.instances.max
-  max_throughput = var.vpc_connector_create.throughput.max
-  min_instances  = var.vpc_connector_create.instances.min
-  min_throughput = var.vpc_connector_create.throughput.min
-  subnet {
-    name       = var.vpc_connector_create.subnet.name
-    project_id = var.vpc_connector_create.subnet.project_id
-  }
-}
-
 resource "google_cloud_run_v2_service" "service" {
   provider     = google-beta
   project      = var.project_id
--- modules/cloud-run-v2/variables.tf
+++ modules/cloud-run-v2/variables.tf
@@ -215,26 +215,3 @@ variable "volumes" {
   default  = {}
   nullable = false
 }
-
-variable "vpc_connector_create" {
-  description = "Populate this to create a Serverless VPC Access connector."
-  type = object({
-    ip_cidr_range = optional(string)
-    vpc_self_link = optional(string)
-    machine_type  = optional(string)
-    name          = optional(string)
-    instances = optional(object({
-      max = optional(number)
-      min = optional(number)
-    }), {})
-    throughput = optional(object({
-      max = optional(number)
-      min = optional(number)
-    }), {})
-    subnet = optional(object({
-      name       = optional(string)
-      project_id = optional(string)
-    }), {})
-  })
-  default = null
-}
