4648e6766c852592c662744a2b70ae0a0a445b1e
Simone Ruffilli <ilruffo@gmail.com>
2023-12-20 09:27:11 +0100

    Initial import of net-sandbox

---
+++ blueprints/networking/net-sandbox/main.tf
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "project" {
+  source          = "../../../modules/project"
+  name            = var.project_id
+  parent          = try(var.project_create_config.parent_id, null)
+  billing_account = try(var.project_create_config.billing_account_id, null)
+  project_create  = try(var.project_create_config.billing_account_id, null) != null
+  prefix          = var.prefix
+  services = [
+    "compute.googleapis.com",
+    "stackdriver.googleapis.com",
+    "networkconnectivity.googleapis.com"
+  ]
+}
