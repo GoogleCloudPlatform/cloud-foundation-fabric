cb9a9ab3733857334205cc64ca4c39800fc188ad
luigi-bitonti <93377317+luigi-bitonti@users.noreply.github.com>
2024-02-07 16:47:49 +0100

    Added destroy_scheduled_duration variable (#2053)
    * Added destroy_scheduled_duration variable
    
    * Fix doc
    
    * Removed default value

--- modules/kms/variables.tf
+++ modules/kms/variables.tf
@@ -78,6 +78,7 @@ variable "keyring_create" {
 variable "keys" {
   description = "Key names and base attributes. Set attributes to null if not needed."
   type = map(object({
+    destroy_scheduled_duration    = optional(string)
     rotation_period               = optional(string)
     labels                        = optional(map(string))
     purpose                       = optional(string, "ENCRYPT_DECRYPT")
