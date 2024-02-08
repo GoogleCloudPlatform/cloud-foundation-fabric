b485aea2da03dd230a1c4e8f70ac433a64dfb98b
Ben Swenka <bswenka@gmail.com>
2024-02-08 10:54:59 -0600

    Fixed linting issues

--- blueprints/networking/psc-glb-and-armor/producer.tf
+++ blueprints/networking/psc-glb-and-armor/producer.tf
@@ -71,9 +71,9 @@ resource "google_compute_region_network_endpoint_group" "neg" {
 }
 
 resource "google_compute_forwarding_rule" "psc_ilb_target_service" {
-  name    = "producer-forwarding-rule"
-  region  = var.region
-  project = module.producer_project.project_id
+  name                  = "producer-forwarding-rule"
+  region                = var.region
+  project               = module.producer_project.project_id
   depends_on            = [google_compute_subnetwork.proxy_subnet]
   load_balancing_scheme = "INTERNAL_MANAGED"
   port_range            = "443"
@@ -165,7 +165,7 @@ resource "google_compute_subnetwork" "proxy_subnet" {
   provider      = google-beta
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
-  project = module.producer_project.project_id
+  project       = module.producer_project.project_id
   purpose       = "REGIONAL_MANAGED_PROXY"
   role          = "ACTIVE"
   network       = google_compute_network.psc_ilb_network.id
