38800a91414f7843f39157fc1ec8a54550c4e0cd
Julio Castillo <jccb@google.com>
2024-02-20 21:21:59 +0100

    Fix typos

--- modules/project/logging.tf
+++ modules/project/logging.tf
@@ -19,7 +19,7 @@
 locals {
   logging_sinks = {
     for k, v in var.logging_sinks :
-    # rewrite destination and type and type="project"
+    # rewrite destination and type when type="project"
     k => merge(v, v.type != "project" ? {} : {
       destination = "projects/${v.destination}"
       type        = "logging"
