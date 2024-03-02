525684faf382194cf31fbe436309467a063876d2
Ludovico Magnocavallo <ludomagno@google.com>
2024-03-02 19:36:29 +0100

    Define service attachment interface for lb modules and implement in internal LBs (#2122)
    * add service attachment support to lb app int module
    
    * allow direct referencing of self managed ig in ilb module
    
    * add service attachment support to net-ilb-int
    
    * add service attachments example to net-lb-int
    
    * fix resource name in net-lb-ext
    
    * rename fwd rules resource in test inventories
    
    * add toc to net-lb-int

--- modules/net-lb-int/outputs.tf
+++ modules/net-lb-int/outputs.tf
@@ -32,7 +32,7 @@ output "backend_service_self_link" {
 output "forwarding_rule_addresses" {
   description = "Forwarding rule address."
   value = {
-    for k, v in google_compute_forwarding_rule.forwarding_rules
+    for k, v in google_compute_forwarding_rule.default
     : k => v.ip_address
   }
 }
@@ -40,7 +40,7 @@ output "forwarding_rule_addresses" {
 output "forwarding_rule_self_links" {
   description = "Forwarding rule self links."
   value = {
-    for k, v in google_compute_forwarding_rule.forwarding_rules
+    for k, v in google_compute_forwarding_rule.default
     : k => v.self_link
   }
 }
@@ -48,7 +48,7 @@ output "forwarding_rule_self_links" {
 output "forwarding_rules" {
   description = "Forwarding rule resources."
   value = {
-    for k, v in google_compute_forwarding_rule.forwarding_rules
+    for k, v in google_compute_forwarding_rule.default
     : k => v
   }
 }
@@ -83,7 +83,14 @@ output "health_check_self_link" {
 output "id" {
   description = "Fully qualified forwarding rule ids."
   value = {
-    for k, v in google_compute_forwarding_rule.forwarding_rules
+    for k, v in google_compute_forwarding_rule.default
     : k => v.id
+  }
+}
+
+output "service_attachment_ids" {
+  description = "Service attachment ids."
+  value = {
+    for k, v in google_compute_service_attachment.default : k => v.id
   }
 }
