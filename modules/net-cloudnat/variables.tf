a590deb58b9919be23d464a9ffec025d6d32d508
Ludovico Magnocavallo <ludomagno@google.com>
2024-03-22 15:59:02 +0100

    Fix subnet configuration in cloud nat module (#2171)
    * support optional secondary ranges in net-cloudnat module
    
    * fix subnet configuration
    
    * fix packer blueprint

--- modules/net-cloudnat/variables.tf
+++ modules/net-cloudnat/variables.tf
@@ -36,10 +36,23 @@ variable "config_port_allocation" {
   }
 }
 
-variable "config_source_subnets" {
-  description = "Subnetwork configuration (ALL_SUBNETWORKS_ALL_IP_RANGES, ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES, LIST_OF_SUBNETWORKS)."
-  type        = string
-  default     = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+variable "config_source_subnetworks" {
+  description = "Subnetwork configuration."
+  type = object({
+    all                 = optional(bool, true)
+    primary_ranges_only = optional(bool)
+    subnetworks = optional(list(object({
+      self_link        = string
+      all_ranges       = optional(bool, true)
+      secondary_ranges = optional(list(string))
+    })), [])
+  })
+  nullable = false
+  default  = {}
+}
+
+output "foo" {
+  value = var.config_source_subnetworks.subnetworks
 }
 
 variable "config_timeouts" {
@@ -109,17 +122,4 @@ variable "rules" {
   }))
   default  = []
   nullable = false
-}
-
-variable "subnetworks" {
-  description = "Subnetworks to NAT, only used when config_source_subnets equals LIST_OF_SUBNETWORKS."
-  type = list(object({
-    self_link     = string
-    all_ip_ranges = optional(bool, true)
-    # secondary ranges are only meaningful when all_ip_ranges is false
-    # in that case we populate config with LIST_OF_SECONDARY_IP_RANGES
-    # only if secondary_ranges is not null
-    secondary_ranges = optional(list(string))
-  }))
-  default = []
 }
