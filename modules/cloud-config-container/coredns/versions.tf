f2806f347b01d3980e7ec6bdd7d107d6656b7473
Wiktor Niesiobędzki <wiktorn@google.com>
2024-03-25 17:20:54 +0000

    Bump provider version to 5.18, so fix non-empty plan for google_notebooks_instance

--- modules/cloud-config-container/coredns/versions.tf
+++ modules/cloud-config-container/coredns/versions.tf
@@ -17,11 +17,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.17.0, < 6.0.0" # tftest
+      version = ">= 5.18.0, < 6.0.0" # tftest
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.17.0, < 6.0.0" # tftest
+      version = ">= 5.18.0, < 6.0.0" # tftest
     }
   }
 }
