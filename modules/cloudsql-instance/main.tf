7930420d603144484ffdf0751b7460a35d4facec
Adrian Lyxell <adrian@sdnit.se>
2024-04-04 11:23:46 +0200

    Added data_cache_config settings for cloudsql-instance module

--- modules/cloudsql-instance/main.tf
+++ modules/cloudsql-instance/main.tf
@@ -67,6 +67,13 @@ resource "google_sql_database_instance" "primary" {
     collation                   = var.collation
     connector_enforcement       = var.connector_enforcement
     time_zone                   = var.time_zone
+    
+    dynamic "data_cache_config" {
+      for_each = var.edition == "ENTERPRISE_PLUS" ? [1] : []
+      content {
+        data_cache_enabled = var.data_cache_enabled
+      }
+    }
 
     ip_configuration {
       ipv4_enabled       = var.network_config.connectivity.public_ipv4
