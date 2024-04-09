6121a9d04d6604146c3656da8da903442945767f
Konrad Schieban <kschieban@google.com>
2024-04-09 14:43:06 +0000

    Readme, variable ordering, refactoring

--- fast/stages/3-gcve/prod/main.tf
+++ fast/stages/3-gcve/prod/main.tf
@@ -56,29 +56,5 @@ module "gcve-pc" {
   }
 
   private_cloud_configs = var.private_cloud_configs
-
-
   gcve_monitoring = var.gcve_monitoring
-
-  /*
-  gcve_monitoring = {
-    vm_mon_name = var.gcve_monitoring.vm_mon_name
-    vm_mon_type = var.gcve_monitoring.vm_mon_type
-    vm_mon_zone = var.gcve_monitoring.vm_mon_zone
-    subnetwork = var.gcve_monitoring.subnetwork
-    sa_gcve_monitoring = var.gcve_monitoring.subnetwork
-    secret_vsphere_server = var.gcve_monitoring.secret_vsphere_server 
-    secret_vsphere_user = var.gcve_monitoring.secret_vsphere_user
-    secret_vsphere_password = var.gcve_monitoring.secret_vsphere_password
-    gcve_region = var.gcve_monitoring.gcve_region
-    hc_interval_sec = var.gcve_monitoring.hc_interval_sec
-    hc_timeout_sec = var.gcve_monitoring.hc_timeout_sec
-    hc_healthy_threshold = var.gcve_monitoring.hc_healthy_threshold
-    hc_unhealthy_threshold = var.gcve_monitoring.hc_unhealthy_threshold
-    initial_delay_sec = var.gcve_monitoring.initial_delay_sec
-    create_dashboards = var.gcve_monitoring.create_dashboards
-    network_project_id = var.host_project_ids.prod-spoke-0
-    network_self_link = var.vpc_self_links.prod-spoke-0
-  }
-  */
 }
