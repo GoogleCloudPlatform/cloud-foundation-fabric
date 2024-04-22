dc010b5ae5b0d9a5c270d677d266619dcd164d70
David White <whitedavid@google.com>
2024-04-22 16:48:09 +0000

    feat: add Assured workloads style folder module.

---
+++ modules/aw-folder/main.tf
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  folder = (
+    var.folder_create
+    ? try(google_assured_workloads_workload.aw_folder[0], null)
+    : try(data.google_folder.folder[0], null)
+  )
+}
+
+data "google_folder" "folder" {
+  count  = var.folder_create ? 0 : 1
+  folder = var.id
+}
+
+resource "google_assured_workloads_workload" "aw_folder" {
+  count  = var.folder_create ? 1 : 0
+  compliance_regime = var.compliance_regime
+  display_name = var.name
+  location = var.location
+  organization = var.parent
+}
+
+resource "google_essential_contacts_contact" "contact" {
+  provider                            = google-beta
+  for_each                            = var.contacts
+  parent                              = local.folder.name
+  email                               = each.key
+  language_tag                        = "en"
+  notification_category_subscriptions = each.value
+  depends_on = [
+    google_folder_iam_binding.authoritative,
+    google_folder_iam_binding.bindings,
+    google_folder_iam_member.bindings
+  ]
+}
+
+resource "google_compute_firewall_policy_association" "default" {
+  count             = var.firewall_policy == null ? 0 : 1
+  attachment_target = local.folder.id
+  name              = var.firewall_policy.name
+  firewall_policy   = var.firewall_policy.policy
+}
