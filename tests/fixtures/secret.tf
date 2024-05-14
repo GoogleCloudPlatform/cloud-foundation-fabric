eb15a75cc1c0e36b52982521e976a0b56b49ff80
Dinesh Kumar Baskar <dibaskar@google.com>
2024-05-14 22:34:23 +0200

    Added license boiler plate

--- tests/fixtures/secret.tf
+++ tests/fixtures/secret.tf
@@ -1,31 +1,45 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 resource "google_project_iam_binding" "bindings" {
-  project  = var.project_id
-  role     = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  members  = ["serviceAccount:${var.service_identities.secret_identity}"]
+  project = var.project_id
+  role    = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members = ["serviceAccount:${var.service_identities.secret_identity}"]
 }
 
 resource "google_kms_key_ring" "keyring_1" {
-  name       = "keyring-1"
-  project    = var.project_id
-  location   = var.region_1
+  name     = "keyring-1"
+  project  = var.project_id
+  location = var.region_1
 }
 
 resource "google_kms_crypto_key" "key_1" {
   name            = "crypto-key-example-1"
   key_ring        = google_kms_key_ring.keyring_1.id
   rotation_period = "100000s"
-  depends_on = [ google_project_iam_binding.bindings ]
+  depends_on      = [google_project_iam_binding.bindings]
 }
 
 resource "google_kms_key_ring" "keyring_gl" {
-  name       = "keyring-gl"
-  project    = var.project_id
-  location   = "global"
+  name     = "keyring-gl"
+  project  = var.project_id
+  location = "global"
 }
 
 resource "google_kms_crypto_key" "key_gl" {
   name            = "crypto-key-example-gl"
   key_ring        = google_kms_key_ring.keyring_gl.id
   rotation_period = "100000s"
-  depends_on = [ google_project_iam_binding.bindings ]
+  depends_on      = [google_project_iam_binding.bindings]
 }
