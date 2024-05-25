245b6d8e8eb8377452054a6098eee6c2806172ad
Ludo <ludomagno@google.com>
2024-05-25 10:16:45 +0200

    cloud run v2

--- modules/cloud-run-v2/tags.tf
+++ modules/cloud-run-v2/tags.tf
@@ -15,9 +15,9 @@
  */
 
 resource "google_tags_location_tag_binding" "binding" {
-  for_each = var.tag_bindings
+  for_each = var.create_job ? {} : var.tag_bindings
   parent = (
-    "//run.googleapis.com/projects/${var.project_id}/locations/europe-west1/services/${google_cloud_run_service.service.name}"
+    "//run.googleapis.com/projects/${var.project_id}/locations/europe-west1/services/${google_cloud_run_v2_service.service[0].name}"
   )
   tag_value = each.value
   location  = var.region
