730f6f2620880109979dca12c5ee251775983574
apichick <mirene@google.com>
2024-08-06 22:14:24 +0200

    Added custom audiences to Cloud Run v2 module

--- modules/cloud-run-v2/service.tf
+++ modules/cloud-run-v2/service.tf
@@ -15,14 +15,15 @@
  */
 
 resource "google_cloud_run_v2_service" "service" {
-  count        = var.create_job ? 0 : 1
-  provider     = google-beta
-  project      = var.project_id
-  location     = var.region
-  name         = "${local.prefix}${var.name}"
-  ingress      = var.ingress
-  labels       = var.labels
-  launch_stage = var.launch_stage
+  count            = var.create_job ? 0 : 1
+  provider         = google-beta
+  project          = var.project_id
+  location         = var.region
+  name             = "${local.prefix}${var.name}"
+  ingress          = var.ingress
+  labels           = var.labels
+  launch_stage     = var.launch_stage
+  custom_audiences = var.custom_audiences
 
   template {
     encryption_key = var.encryption_key
