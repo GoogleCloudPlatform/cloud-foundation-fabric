dff209f5651183882d65acca53347513bcbfb9c2
arnodasilva <6975187+arnodasilva@users.noreply.github.com>
2024-08-20 07:43:18 +0200

    fix(modules/kms): use the `google_tags_location_tag_binding` Terraform resource to bind tags on KMS key rings (#2501)

--- modules/kms/tags.tf
+++ modules/kms/tags.tf
@@ -14,8 +14,9 @@
  * limitations under the License.
  */
 
-resource "google_tags_tag_binding" "binding" {
+resource "google_tags_location_tag_binding" "binding" {
   for_each  = var.tag_bindings
   parent    = "//cloudkms.googleapis.com/${local.keyring.id}"
   tag_value = each.value
+  location  = var.keyring.location
 }
