eb89c62b5f814c6b61eb5a55e7e7278afeed9c80
Ludovico Magnocavallo <ludomagno@google.com>
2024-08-30 10:06:33 +0200

    prepare v34.0.0 release (#2543)

--- fast/stages/1-resman/variables-fast.tf
+++ fast/stages/1-resman/variables-fast.tf
@@ -54,8 +54,9 @@ variable "custom_roles" {
   # tfdoc:variable:source 0-bootstrap
   description = "Custom roles defined at the org level, in key => id format."
   type = object({
-    gcve_network_admin               = string
-    network_firewall_policies_admin  = string
+    gcve_network_admin              = string
+    network_firewall_policies_admin = string
+    # TODO: remove after v34.0.0
     network_firewall_policies_viewer = string
     ngfw_enterprise_admin            = string
     ngfw_enterprise_viewer           = string
