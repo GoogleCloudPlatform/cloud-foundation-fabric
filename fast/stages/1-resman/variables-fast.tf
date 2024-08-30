3f2e87f72e23e5ba62e0664a99258bea40c80f84
Ludo <ludomagno@google.com>
2024-08-30 07:39:11 +0200

    moved blocks and fixes for FAST v33-v34 transition

--- fast/stages/1-resman/variables-fast.tf
+++ fast/stages/1-resman/variables-fast.tf
@@ -54,13 +54,14 @@ variable "custom_roles" {
   # tfdoc:variable:source 0-bootstrap
   description = "Custom roles defined at the org level, in key => id format."
   type = object({
-    gcve_network_admin              = string
-    network_firewall_policies_admin = string
-    ngfw_enterprise_admin           = string
-    ngfw_enterprise_viewer          = string
-    organization_admin_viewer       = string
-    service_project_network_admin   = string
-    storage_viewer                  = string
+    gcve_network_admin               = string
+    network_firewall_policies_admin  = string
+    network_firewall_policies_viewer = string
+    ngfw_enterprise_admin            = string
+    ngfw_enterprise_viewer           = string
+    organization_admin_viewer        = string
+    service_project_network_admin    = string
+    storage_viewer                   = string
   })
   default = null
 }
