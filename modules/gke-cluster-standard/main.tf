76754e31fbf332fc9b3656d35c4ef3892e662061
oluakingcp <148869667+oluakingcp@users.noreply.github.com>
2024-09-03 11:40:44 +0100

    Update main.tf for security_posture_config
    Updating this module for security posture config / vulnerability mode

--- modules/gke-cluster-standard/main.tf
+++ modules/gke-cluster-standard/main.tf
@@ -477,6 +477,13 @@ resource "google_container_cluster" "cluster" {
       enabled = var.enable_features.pod_security_policy
     }
   }
+  dynamic "security_posture_config" {
+    for_each = var.enable_features.security_posture_config != null ? [""] : []
+    content {
+      mode                = var.enable_features.security_posture_config.mode
+      vulnerability_mode  = var.enable_features.security_posture_config.vulnerability_mode
+    }
+  }
   dynamic "release_channel" {
     for_each = var.release_channel != null ? [""] : []
     content {
