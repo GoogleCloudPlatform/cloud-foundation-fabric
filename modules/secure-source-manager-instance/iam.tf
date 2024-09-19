0f28d266dd04898fb448a0246a2ab837da79fb69
apichick <mirene@google.com>
2024-09-19 12:29:01 +0200

    Secure source manager (#2569)
    * Added secure-source-manager-instance module
    
    * Fixes after review

---
+++ modules/secure-source-manager-instance/iam.tf
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  repository_iam = merge([for k1, v1 in var.repositories : { for k2, v2 in v1.iam :
+    "${k1}.${k2}" => {
+      repository = k1
+      role       = k2
+      members    = v2
+  } }]...)
+  repository_iam_bindings = merge([for k1, v1 in var.repositories : { for k2, v2 in v1.iam_bindings :
+    "${k1}.${k2}" => merge(v2, {
+      repository = k1
+  }) }]...)
+  repository_iam_bindings_additive = merge([for k1, v1 in var.repositories : { for k2, v2 in v1.iam_bindings_additive :
+    "${k1}.${k2}" => merge(v2, {
+      repository = k1
+  }) }]...)
+}
+
+resource "google_secure_source_manager_instance_iam_binding" "authoritative" {
+  for_each    = var.iam
+  project     = google_secure_source_manager_instance.instance.project
+  instance_id = google_secure_source_manager_instance.instance.instance_id
+  role        = each.key
+  members     = each.value
+}
+
+resource "google_secure_source_manager_instance_iam_binding" "bindings" {
+  for_each    = var.iam_bindings
+  project     = google_secure_source_manager_instance.instance.project
+  instance_id = google_secure_source_manager_instance.instance.instance_id
+  role        = each.value.role
+  members     = each.value.members
+}
+
+resource "google_secure_source_manager_instance_iam_member" "bindings" {
+  for_each    = var.iam_bindings_additive
+  project     = google_secure_source_manager_instance.instance.project
+  instance_id = google_secure_source_manager_instance.instance.instance_id
+  role        = each.value.role
+  member      = each.value.member
+}
+
+resource "google_secure_source_manager_repository_iam_binding" "authoritative" {
+  for_each      = local.repository_iam
+  project       = google_secure_source_manager_repository.repositories[each.value.repository].project
+  repository_id = google_secure_source_manager_repository.repositories[each.value.repository].repository_id
+  role          = each.value.role
+  members       = each.value.members
+}
+
+resource "google_secure_source_manager_repository_iam_binding" "bindings" {
+  for_each      = local.repository_iam_bindings
+  project       = google_secure_source_manager_repository.repositories[each.value.repository].project
+  repository_id = google_secure_source_manager_repository.repositories[each.value.repository].repository_id
+  role          = each.value.role
+  members       = each.value.members
+}
+
+resource "google_secure_source_manager_repository_iam_member" "bindings" {
+  for_each      = local.repository_iam_bindings_additive
+  project       = google_secure_source_manager_repository.repositories[each.value.repository].project
+  repository_id = google_secure_source_manager_repository.repositories[each.value.repository].repository_id
+  role          = each.value.role
+  member        = each.value.member
+}
