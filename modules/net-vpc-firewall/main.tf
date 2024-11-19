6595e772c47800d6f3a30955850f6ea6d50d606b
Luca Prete <lucaprete@google.com>
2024-11-19 14:15:44 +0100

    Protect against empty files as well

--- modules/net-vpc-firewall/main.tf
+++ modules/net-vpc-firewall/main.tf
@@ -24,7 +24,7 @@ locals {
   # decode rule files and account for optional attributes
   _factory_rule_list = flatten([
     for f in local._factory_rule_files : [
-      for direction, ruleset in yamldecode(file(f)) : [
+      for direction, ruleset in coalesce(yamldecode(file(f)), tomap({})) : [
         for name, rule in coalesce(ruleset, tomap({})) : {
           name                 = name
           deny                 = try(rule.deny, false)
