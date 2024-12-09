8dbac47fcebca7f077b653c1042f58ce03b6a770
Matthew Callinan <47421139+Mattible@users.noreply.github.com>
2024-12-09 18:48:23 +1100

    remove default location in tag value - cloud-run-v2 tags.tf (#2755)
    The Parent resource has a default to europe-west1 when it should be for the resource block from where the cloud run actually is.
    
    Changed to use the var.region instead

--- modules/cloud-run-v2/tags.tf
+++ modules/cloud-run-v2/tags.tf
@@ -17,7 +17,7 @@
 resource "google_tags_location_tag_binding" "binding" {
   for_each = var.create_job ? {} : var.tag_bindings
   parent = (
-    "//run.googleapis.com/projects/${var.project_id}/locations/europe-west1/services/${google_cloud_run_v2_service.service[0].name}"
+    "//run.googleapis.com/projects/${var.project_id}/locations/${var.region}/services/${google_cloud_run_v2_service.service[0].name}"
   )
   tag_value = each.value
   location  = var.region
