0f1070329cb3988a0c224897cde2b337d5238cd8
Julio Castillo <jccb@google.com>
2025-01-12 21:26:38 +0100

    Fix linting

--- tools/versions.py
+++ tools/versions.py
@@ -68,8 +68,10 @@ def process_file(file_path, context):
 
 @click.command()
 @click.option("--fabric-release", help="Override Fabric release version")
-@click.option("--provider-min-version", help="Override GCP provider min version")
-@click.option("--provider-max-version", help="Override GCP provider max version")
+@click.option("--provider-min-version",
+              help="Override GCP provider min version")
+@click.option("--provider-max-version",
+              help="Override GCP provider max version")
 @click.option("--tf-version", help="Override Terraform version")
 @click.option("--tofu-version", help="Override OpenTofu version")
 @click.option("--write-defaults/--no-write-defaults", default=False,
