396175bf700528a056090e2ef4e05a687ab3330a
Julio Castillo <jccb@google.com>
2025-01-14 00:22:13 +0100

    First attempt at iam_by_principals_additive

--- modules/folder/variables-iam.tf
+++ modules/folder/variables-iam.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +49,51 @@ variable "iam_bindings_additive" {
   }))
   nullable = false
   default  = {}
+}
+
+variable "iam_by_principals_additive" {
+  description = "Additive IAM binding in {PRINCIPAL => [ROLES]} format. Principals need to be statically defined to avoid cycle errors."
+  type        = map(list(string))
+  default     = {}
+  nullable    = false
+
+  validation {
+    error_message = format(
+      "Cannot refer to the same principal/role pair from iam_by_principals_additive and iam_bindings_additive. The following pairs are present on both sides:\n%s",
+      join("\n", [
+        // the iterable here is the same as the condition expression below.
+        // We duplicate it here to show the offending pairs in the error.
+        for x in setintersection(
+          [
+            for k, v in var.iam_bindings_additive :
+            [v.member, v.role]
+            if v.condition == null
+          ],
+          concat([
+            for k, v in var.iam_by_principals_additive :
+            setproduct([k], v)
+          ]...),
+        ) :
+        "${x.0} ${x.1}"
+      ])
+    )
+    condition = (
+      0 == length(setintersection(
+        // the set of all additive bindings without conditions
+        [
+          for k, v in var.iam_bindings_additive :
+          [v.member, v.role]
+          if v.condition == null
+        ],
+        // the set of all pairs of all pairs principal x role
+        concat([
+          for k, v in var.iam_by_principals_additive :
+          setproduct([k], v)
+        ]...)
+      ))
+    )
+  }
+
 }
 
 variable "iam_by_principals" {
