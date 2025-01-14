8aad22adec8e9ba5f48ed31797ba1e5e5f771163
Julio Castillo <jccb@google.com>
2025-01-14 15:53:40 +0100

    Add missing comma

--- modules/project-factory/factory-projects.tf
+++ modules/project-factory/factory-projects.tf
@@ -158,7 +158,7 @@ locals {
         )
       )
       logging_data_access = coalesce(
-        var.data_overrides.logging_data_access
+        var.data_overrides.logging_data_access,
         try(v.logging_data_access),
         var.data_defaults.logging_data_access
       )
