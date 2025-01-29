0abb9689e6326af6a99d4bddd9da8b9a30fb644e
Julio Castillo <jccb@google.com>
2025-01-29 07:03:53 +0100

    Add principal interpolation to iam_by_principals (#2847)
    * Add principal interpolation to iam_by_principals
    
    * Fix tests

--- fast/stages/1-resman/top-level-folders.tf
+++ fast/stages/1-resman/top-level-folders.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,7 +86,8 @@ module "top-level-folder" {
   iam = {
     for role, members in each.value.iam :
     lookup(var.custom_roles, role, role) => [
-      for member in members : (each.value.automation != null && member == "self")
+      for member in members :
+      (each.value.automation != null && member == "self")
       ? module.top-level-sa[each.key].iam_email
       : lookup(local.principals_iam, member, member)
     ]
@@ -94,7 +95,8 @@ module "top-level-folder" {
   iam_bindings = {
     for k, v in each.value.iam_bindings : k => {
       members = [
-        for member in v.members : (each.value.automation != null && member == "self")
+        for member in v.members :
+        (each.value.automation != null && member == "self")
         ? module.top-level-sa[each.key].iam_email
         : lookup(local.top_level_sa, member, member)
       ]
@@ -111,9 +113,15 @@ module "top-level-folder" {
       role = lookup(var.custom_roles, v.role, v.role)
     })
   }
-  # we don't replace here to avoid dynamic values in keys
-  iam_by_principals = each.value.iam_by_principals
-  org_policies      = each.value.org_policies
+  iam_by_principals = {
+    for k, v in each.value.iam_by_principals :
+    (
+      (each.value.automation != null && k == "self")
+      ? module.top-level-sa[each.key].iam_email
+      : lookup(local.principals_iam, k, k)
+    ) => [for r in v : lookup(var.custom_roles, r, r)]
+  }
+  org_policies = each.value.org_policies
   tag_bindings = merge(
     # explicit tag bindings
     {
