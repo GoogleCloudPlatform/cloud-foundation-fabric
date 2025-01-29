95ec5ee3b5b7ba6194c454fbc2bb7bd06dc694f7
Ludovico Magnocavallo <ludomagno@google.com>
2025-01-29 13:16:35 +0100

    Flexible stage 2s in FAST resource manager (#2840)
    * wip
    
    * WIP
    
    * wip
    
    * wip
    
    * apply untested
    
    * tests
    
    * support tag expansion for tenant-level installations in IAM conditions
    
    * fix stage config output
    
    * inventories
    
    * remove dev files
    
    * tfdoc
    
    * enable org policies for stage folders
    
    * resman README
    
    * tfdoc
    
    * stage 3 documentation
    
    * inventory
    
    * support extra_dirs in testing franework
    
    * remove org policy files from stage 1
    
    * Add principal interpolation to iam_by_principals (#2847)
    
    * Add principal interpolation to iam_by_principals
    
    * Fix tests
    
    * relax schemas
    
    * relax schemas
    
    ---------
    
    Co-authored-by: Julio Castillo <jccb@google.com>

--- fast/stages/1-resman/stage-2.tf
+++ fast/stages/1-resman/stage-2.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,28 +14,29 @@
  * limitations under the License.
  */
 
+# tfdoc:file:description Stage 2s locals and resources.
+
 locals {
   # read and decode factory files
-  _stage3_path = try(
-    pathexpand(var.factories_config.stage_3), null
+  _stage2_path = try(
+    pathexpand(var.factories_config.stage_2), null
   )
-  _stage3_files = try(
-    fileset(local._stage3_path, "**/*.yaml"),
+  _stage2_files = try(
+    fileset(local._stage2_path, "**/*.yaml"),
     []
   )
-  _stage3_data = {
-    for f in local._stage3_files :
+  _stage2_data = {
+    for f in local._stage2_files :
     split(".", f)[0] => yamldecode(file(
-      "${coalesce(local._stage3_path, "-")}/${f}"
+      "${coalesce(local._stage2_path, "-")}/${f}"
     ))
   }
-  # merge stage 3 from factory and variable data
-  _stage3 = merge(
+  # merge stage 2 from factory and variable data
+  _stage2 = merge(
     # normalize factory data attributes with defaults and nulls
     {
-      for k, v in local._stage3_data : k => {
-        short_name  = v.short_name
-        environment = try(v.environment, "dev")
+      for k, v in local._stage2_data : k => {
+        short_name = lookup(v, "short_name", null)
         cicd_config = lookup(v, "cicd_config", null) == null ? null : {
           identity_provider = v.cicd_config.identity_provider
           repository = merge(v.cicd_config.repository, {
@@ -45,23 +46,32 @@ locals {
         }
         folder_config = lookup(v, "folder_config", null) == null ? null : {
           name                  = v.folder_config.name
+          create_env_folders    = try(v.folder_config.create_env_folders, false)
           iam                   = try(v.folder_config.iam, {})
           iam_bindings          = try(v.folder_config.iam_bindings, {})
           iam_bindings_additive = try(v.folder_config.iam_bindings_additive, {})
           iam_by_principals     = try(v.folder_config.iam_by_principals, {})
           org_policies          = try(v.folder_config.org_policies, {})
           parent_id             = try(v.folder_config.parent_id, null)
-          tag_bindings          = try(v.folder_config.tag_bindings, {})
+        }
+        organization_config = {
+          iam                   = try(v.organization_config.iam, {})
+          iam_bindings          = try(v.organization_config.iam_bindings, {})
+          iam_bindings_additive = try(v.organization_config.iam_bindings_additive, {})
+          iam_by_principals     = try(v.organization_config.iam_by_principals, {})
+        }
+        stage3_config = {
+          iam_admin_delegated = try(v.stage3_config.iam_admin_delegated, [])
+          iam_viewer          = try(v.stage3_config.iam_viewer, [])
         }
       }
     },
-    var.fast_stage_3
+    var.fast_stage_2
   )
   # normalize attributes
-  stage3 = {
-    for k, v in local._stage3 : k => merge(v, {
+  stage2 = {
+    for k, v in local._stage2 : k => merge(v, {
       short_name = replace(coalesce(v.short_name, k), "_", "-")
-      # this code is identical to the one used for stage 2s
       folder_config = v.folder_config == null ? null : merge(v.folder_config, {
         iam = {
           for kk, vv in v.folder_config.iam : kk => [
@@ -75,7 +85,7 @@ locals {
             members = [
               for m in vv.members : contains(["ro", "rw"], m) ? "${k}-${m}" : m
             ]
-            condition = vv.condition == null ? null : {
+            condition = lookup(vv, "condition", null) == null ? null : {
               title = vv.condition.title
               expression = templatestring(vv.condition.expression, {
                 organization = var.organization
@@ -91,7 +101,28 @@ locals {
           kk => {
             role   = vv.role
             member = contains(["ro", "rw"], vv.member) ? "${k}-${vv.member}" : vv.member
-            condition = vv.condition == null ? null : {
+            condition = lookup(vv, "condition", null) == null ? null : {
+              title = vv.condition.title
+              expression = templatestring(vv.condition.expression, {
+                organization = var.organization
+                tag_names    = var.tag_names
+                tag_root     = local.tag_root
+              })
+              description = lookup(vv.condition, "description", null)
+            }
+          }
+        }
+        iam_by_principals = {
+          for kk, vv in v.folder_config.iam_by_principals :
+          (contains(["ro", "rw"], kk) ? "${k}-${kk}" : kk) => vv
+        }
+      })
+      organization_config = merge(v.organization_config, {
+        iam_bindings_additive = {
+          for kk, vv in v.organization_config.iam_bindings_additive : kk => {
+            member = contains(["ro", "rw"], vv.member) ? "${k}-${vv.member}" : vv.member
+            role   = vv.role
+            condition = lookup(vv, "condition", null) == null ? null : {
               title = vv.condition.title
               expression = templatestring(vv.condition.expression, {
                 organization = var.organization
@@ -104,29 +135,28 @@ locals {
         }
       })
     })
-    if !contains(
-      local.stage2_shortnames, replace(coalesce(v.short_name, k), "_", "-")
-    )
   }
-}
-
-check "stage_short_names" {
-  assert {
-    condition = alltrue([
-      for k, v in local._stage3 : !contains(
-        local.stage2_shortnames, replace(coalesce(v.short_name, k), "_", "-")
-      )
-    ])
-    error_message = "Some stage 3 short names overlap stage 2."
-  }
+  # environment folder permutations
+  stage2_env_folders = flatten([
+    for k, v in local.stage2 : [
+      for ek, ev in var.environments : {
+        key      = "${k}-${ek}"
+        name     = ev.name
+        stage    = k
+        tag_name = ev.tag_name
+      }
+    ] if try(v.folder_config.create_env_folders, null) == true
+  ])
+  # stage 2 short names used to detect overlap in stage 3s
+  stage2_shortnames = [for k, v in local.stage2 : v.short_name]
 }
 
 # top-level folder
 
-module "stage3-folder" {
+module "stage2-folder" {
   source = "../../../modules/folder"
   for_each = {
-    for k, v in local.stage3 : k => v if v.folder_config != null
+    for k, v in local.stage2 : k => v if v.folder_config != null
   }
   parent = (
     each.value.folder_config.parent_id == null
@@ -138,43 +168,66 @@ module "stage3-folder" {
   )
   name = each.value.folder_config.name
   iam = {
-    "roles/logging.admin"                  = [module.stage3-sa-rw[each.key].iam_email]
-    "roles/owner"                          = [module.stage3-sa-rw[each.key].iam_email]
-    "roles/resourcemanager.folderAdmin"    = [module.stage3-sa-rw[each.key].iam_email]
-    "roles/resourcemanager.projectCreator" = [module.stage3-sa-rw[each.key].iam_email]
-    "roles/compute.xpnAdmin"               = [module.stage3-sa-rw[each.key].iam_email]
-    "roles/viewer"                         = [module.stage3-sa-ro[each.key].iam_email]
-    "roles/resourcemanager.folderViewer"   = [module.stage3-sa-ro[each.key].iam_email]
-
+    for k, v in each.value.folder_config.iam :
+    lookup(var.custom_roles, k, k) => [
+      for m in v : lookup(local.principals_iam, m, m)
+    ]
   }
-  iam_by_principals = each.value.folder_config.iam_by_principals
-  org_policies      = each.value.folder_config.org_policies
-  tag_bindings = merge(
-    {
-      environment = local.tag_values["environment/${var.environments[each.value.environment].tag_name}"].id
-    },
-    {
-      for k, v in each.value.folder_config.tag_bindings : k => try(
-        local.tag_values[v].id, v
-      )
-    }
-  )
+  iam_bindings = {
+    for k, v in each.value.folder_config.iam_bindings : k => merge(v, {
+      members = [
+        for m in v.members : lookup(local.principals_iam, m, m)
+      ]
+      role = lookup(var.custom_roles, v.role, v.role)
+    })
+  }
+  iam_bindings_additive = {
+    for k, v in each.value.folder_config.iam_bindings_additive : k => merge(v, {
+      member = lookup(local.principals_iam, v.member, v.member)
+      role   = lookup(var.custom_roles, v.role, v.role)
+    })
+  }
+  iam_by_principals = {
+    for k, v in each.value.folder_config.iam_by_principals :
+    lookup(local.principals_iam, k, k) => [
+      for r in v : lookup(var.custom_roles, r, r)
+    ]
+  }
+  org_policies = each.value.folder_config.org_policies
+  tag_bindings = {
+    context = local.tag_values["context/${each.key}"].id
+  }
   depends_on = [module.top-level-folder]
+}
+
+# optional per-environment folders
+
+module "stage2-folder-env" {
+  source   = "../../../modules/folder"
+  for_each = { for k in local.stage2_env_folders : k.key => k }
+  parent   = module.stage2-folder[each.value.stage].id
+  name     = each.value.name
+  tag_bindings = {
+    environment = try(
+      local.tag_values["${var.tag_names.environment}/${each.value.tag_name}"].id,
+      null
+    )
+  }
 }
 
 # automation service accounts
 
-module "stage3-sa-rw" {
+module "stage2-sa-rw" {
   source     = "../../../modules/iam-service-account"
-  for_each   = local.stage3
+  for_each   = local.stage2
   project_id = var.automation.project_id
-  name = templatestring(var.resource_names["sa-stage3_rw"], {
+  name = templatestring(var.resource_names["sa-stage2_rw"], {
     name = each.value.short_name
   })
   display_name = (
     "Terraform resman ${each.key} service account."
   )
-  prefix = "${var.prefix}-${var.environments[each.value.environment].short_name}"
+  prefix = "${var.prefix}-${local.environment_default.short_name}"
   iam = {
     "roles/iam.serviceAccountTokenCreator" = compact([
       try(module.cicd-sa-rw["${each.key}-prod"].iam_email, null)
@@ -188,17 +241,17 @@ module "stage3-sa-rw" {
   }
 }
 
-module "stage3-sa-ro" {
+module "stage2-sa-ro" {
   source     = "../../../modules/iam-service-account"
-  for_each   = local.stage3
+  for_each   = local.stage2
   project_id = var.automation.project_id
-  name = templatestring(var.resource_names["sa-stage3_ro"], {
+  name = templatestring(var.resource_names["sa-stage2_ro"], {
     name = each.value.short_name
   })
   display_name = (
     "Terraform resman ${each.key} service account (read-only)."
   )
-  prefix = "${var.prefix}-${each.value.environment}"
+  prefix = "${var.prefix}-${local.environment_default.short_name}"
   iam = {
     "roles/iam.serviceAccountTokenCreator" = compact([
       try(module.cicd-sa-ro["${each.key}-prod"].iam_email, null)
@@ -214,18 +267,18 @@ module "stage3-sa-ro" {
 
 # automation bucket
 
-module "stage3-bucket" {
+module "stage2-bucket" {
   source     = "../../../modules/gcs"
-  for_each   = local.stage3
+  for_each   = local.stage2
   project_id = var.automation.project_id
-  name = templatestring(var.resource_names["gcs-stage3"], {
+  name = templatestring(var.resource_names["gcs-stage2"], {
     name = each.value.short_name
   })
-  prefix     = "${var.prefix}-${each.value.environment}"
+  prefix     = "${var.prefix}-${local.environment_default.short_name}"
   location   = var.locations.gcs
   versioning = true
   iam = {
-    "roles/storage.objectAdmin"  = [module.stage3-sa-rw[each.key].iam_email]
-    "roles/storage.objectViewer" = [module.stage3-sa-ro[each.key].iam_email]
+    "roles/storage.objectAdmin"  = [module.stage2-sa-rw[each.key].iam_email]
+    "roles/storage.objectViewer" = [module.stage2-sa-ro[each.key].iam_email]
   }
 }
