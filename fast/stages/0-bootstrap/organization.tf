30bcb983c5b97d54d94a1b403c5137bead2b4170
Julio Castillo <jccb@google.com>
2025-01-30 21:51:20 +0100

    Expose custom constraint factory in bootstrap

--- fast/stages/0-bootstrap/organization.tf
+++ fast/stages/0-bootstrap/organization.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -198,6 +198,9 @@ module "organization" {
   custom_roles = var.custom_roles
   factories_config = {
     custom_roles = var.factories_config.custom_roles
+    org_policy_custom_constraints = (
+      var.bootstrap_user != null ? null : var.factories_config.custom_constraints
+    )
     org_policies = (
       var.bootstrap_user != null ? null : var.factories_config.org_policies
     )
