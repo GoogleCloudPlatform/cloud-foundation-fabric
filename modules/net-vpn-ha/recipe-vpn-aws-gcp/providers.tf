d4601e714550e82ee03a24c09d4730d61b02aeef
Julio Castillo <jccb@google.com>
2025-04-14 09:39:16 +0200

    Update providers to work without credentials

--- modules/net-vpn-ha/recipe-vpn-aws-gcp/providers.tf
+++ modules/net-vpn-ha/recipe-vpn-aws-gcp/providers.tf
@@ -16,4 +16,11 @@
 
 provider "aws" {
   region = var.aws_region
+  # The values below are needed to allow the tests to work for this
+  # recipe without requiring credentials
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
