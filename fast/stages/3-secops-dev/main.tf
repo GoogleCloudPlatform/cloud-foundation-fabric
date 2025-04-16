2ff01b179b629ef17f008a9d814a532ceaf60432
bruzzechesse <bruzzechesse@google.com>
2025-04-16 11:18:05 +0200

    move project-template in 3-secops-dev

--- fast/stages/3-secops-dev/main.tf
+++ fast/stages/3-secops-dev/main.tf
@@ -14,16 +14,91 @@
  * limitations under the License.
  */
 
-module "secops-tenant" {
-  source = "../../project-templates/secops-tenant"
-  project_id = var.secops_project_ids[var.stage.environment]
-  secops_group_principals = var.secops_group_principals
-  secops_iam = var.secops_iam
-  secops_tenant_config = var.secops_tenant_config
-  secops_data_rbac_config = var.secops_data_rbac_config
-  project_create_config = var.project_create_config
-  regions = var.regions
-  workspace_integration_config = var.workspace_integration_config
+locals {
+  secops_api_key_secret_key   = "secops-feeds-api-key"
+  secops_workspace_int_sa_key = "secops-workspace-ing-sa-key"
+  secops_feeds_api_path       = "projects/${module.project.project_id}/locations/${var.secops_tenant_config.region}/instances/${var.secops_tenant_config.customer_id}/feeds"
+  workspace_log_ingestion     = var.workspace_integration_config != null
+}
+
+module "project" {
+  source          = "../../../modules/project"
+  name            = var.secops_project_ids[var.stage.environment]
+  billing_account = try(var.project_create_config.billing_account, null)
+  parent          = try(var.project_create_config.parent, null)
+  project_reuse   = var.project_create_config != null ? null : {}
+  org_policies = var.workspace_integration_config != null ? {
+    "iam.disableServiceAccountKeyCreation" = {
+      rules = [{ enforce = false }]
+    }
+  } : {}
+  services = concat([
+    "apikeys.googleapis.com",
+    "compute.googleapis.com",
+    "iap.googleapis.com",
+    "secretmanager.googleapis.com",
+    "stackdriver.googleapis.com",
+    "pubsub.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    ],
+    var.workspace_integration_config != null ? [
+      "admin.googleapis.com",
+      "alertcenter.googleapis.com"
+    ] : [],
+  )
+  custom_roles = {
+    "secopsDashboardViewer" = [
+      "chronicle.dashboardCharts.get",
+      "chronicle.dashboardCharts.list",
+      "chronicle.dashboardQueries.execute",
+      "chronicle.dashboardQueries.get",
+      "chronicle.dashboardQueries.list",
+      "chronicle.dashboards.get",
+      "chronicle.dashboards.list",
+      "chronicle.dashboards.schedule",
+      "chronicle.nativeDashboards.get",
+      "chronicle.nativeDashboards.list"
+    ]
+    "secopsDataViewer" = [
+      "chronicle.legacies.legacyFindRawLogs",
+      "chronicle.legacies.legacySearchRawLogs",
+      "chronicle.referenceLists.get",
+      "chronicle.referenceLists.update"
+    ]
+  }
+  iam = {
+    "roles/chronicle.viewer" = compact(concat(
+      [for group in var.secops_group_principals.viewers : "group:${group}"]
+    ))
+  }
+  iam_bindings_additive = merge({ for group in var.secops_group_principals.admins : group => {
+    member = "group:${group}"
+    role   = "roles/chronicle.admin"
+    }
+    }, merge({
+      for k, v in var.secops_iam : k => {
+        member = k
+        role   = "roles/chronicle.restrictedDataAccess"
+        condition = {
+          expression  = join(" || ", [for scope in v.scopes : "resource.name.endsWith('/${scope}')"])
+          title       = "datarbac"
+          description = "datarbac"
+        }
+      }
+  }))
+  iam_by_principals_additive = { for k, v in var.secops_iam : k => v.roles }
+}
+
+resource "google_apikeys_key" "feed_api_key" {
+  project      = module.project.project_id
+  name         = "secops-feed-key"
+  display_name = "SecOps Feeds API Key"
+
+  restrictions {
+    api_targets {
+      service = "chronicle.googleapis.com"
+    }
+  }
 }
 
 module "secops-rules" {
