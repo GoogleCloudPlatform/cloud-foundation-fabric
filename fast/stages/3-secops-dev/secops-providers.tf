f2b86eeec67e51a9a9f69e9f32185ed1cfcf2ca0
bruzzechesse <bruzzechesse@google.com>
2025-04-18 13:40:36 +0200

    fix

--- fast/stages/3-secops-dev/secops-providers.tf
+++ fast/stages/3-secops-dev/secops-providers.tf
@@ -14,14 +14,16 @@
  * limitations under the License.
  */
 
-data "google_client_config" "default" {}
+data "google_client_config" "default" {
+  count = var.project_reuse == null ? 0 : 1
+}
 
 provider "restful" {
   base_url = "https://${var.tenant_config.region}-chronicle.googleapis.com/v1alpha/"
   security = {
     http = {
       token = {
-        token = try(data.google_client_config.default.access_token,"")
+        token = try(data.google_client_config.default[0].access_token, "")
       }
     }
   }
