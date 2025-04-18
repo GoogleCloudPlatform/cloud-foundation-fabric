c65a8afa6fafb9342f4deac591d052bfe7891876
Ludo <ludomagno@google.com>
2025-04-18 17:28:25 +0200

    streamline vpc-sc moved file

--- fast/stages/1-vpcsc/moved/v38.0.0-v39.0.0.tf
+++ fast/stages/1-vpcsc/moved/v38.0.0-v39.0.0.tf
@@ -15,16 +15,6 @@
  */
 
 moved {
-  from = module.vpc-sc[0].google_access_context_manager_service_perimeter.regular["default"]
-  to   = module.vpc-sc.google_access_context_manager_service_perimeter.regular["default"]
-}
-
-moved {
-  from = module.vpc-sc[0].google_access_context_manager_access_level.basic["geo"]
-  to   = module.vpc-sc.google_access_context_manager_access_level.basic["geo"]
-}
-
-moved {
-  from = module.vpc-sc[0].google_access_context_manager_access_policy.default[0]
-  to   = module.vpc-sc.google_access_context_manager_access_policy.default[0]
+  from = module.vpc-sc[0]
+  to   = module.vpc-sc
 }
