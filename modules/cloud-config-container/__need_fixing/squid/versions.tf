77bd504e0082f3894d8c749767ab19bad07bf066
Julio Castillo <jccb@google.com>
2025-05-21 15:01:37 +0200

    Fix versions.tf module path

--- modules/cloud-config-container/__need_fixing/squid/versions.tf
+++ modules/cloud-config-container/__need_fixing/squid/versions.tf
@@ -27,9 +27,9 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "google-pso-tool/cloud-foundation-fabric/path:v40.0.0-tf"
+    module_name = "google-pso-tool/cloud-foundation-fabric/modules/cloud-config-container/__need_fixing/squid:v40.0.0-tf"
   }
   provider_meta "google-beta" {
-    module_name = "google-pso-tool/cloud-foundation-fabric/path:v40.0.0-tf"
+    module_name = "google-pso-tool/cloud-foundation-fabric/modules/cloud-config-container/__need_fixing/squid:v40.0.0-tf"
   }
 }
