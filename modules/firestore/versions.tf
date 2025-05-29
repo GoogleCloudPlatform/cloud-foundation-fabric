13190232781a41ba5b67e899f8003f1c524252e4
Julio Castillo <jccb@google.com>
2025-05-29 09:26:53 +0200

    Bump Terraform version to 1.11

--- modules/firestore/versions.tf
+++ modules/firestore/versions.tf
@@ -15,7 +15,7 @@
 # Fabric release: 40.1.0
 
 terraform {
-  required_version = ">= 1.10.2"
+  required_version = ">= 1.11.4"
   required_providers {
     google = {
       source  = "hashicorp/google"
