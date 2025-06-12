5cbf3d0ef5c431c9500e05026b99ebdee4c043ff
Martin Schinz <martin.schinz@upvest.co>
2025-06-12 17:44:37 +0200

    fix: missing mount_options cloud-run-v2 gcs volume

--- modules/cloud-run-v2/service.tf
+++ modules/cloud-run-v2/service.tf
@@ -221,8 +221,9 @@ resource "google_cloud_run_v2_service" "service" {
         dynamic "gcs" {
           for_each = volumes.value.gcs == null ? [] : [""]
           content {
-            bucket    = volumes.value.gcs.bucket
-            read_only = volumes.value.gcs.is_read_only
+            bucket        = volumes.value.gcs.bucket
+            read_only     = volumes.value.gcs.is_read_only
+            mount_options = volumes.value.gcs.mount_options
           }
         }
         dynamic "nfs" {
