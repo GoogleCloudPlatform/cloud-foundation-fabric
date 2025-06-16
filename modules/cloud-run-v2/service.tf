549988b88397a25b48d0411d606738695dff4dd5
Maciej Sikora <maciej.sikora@rtbhouse.com>
2025-06-16 09:42:48 +0200

    ignores all labels in unmanaged version

--- modules/cloud-run-v2/service.tf
+++ modules/cloud-run-v2/service.tf
@@ -502,9 +502,7 @@ resource "google_cloud_run_v2_service" "service_unmanaged" {
       client_version,
       template[0].annotations["run.googleapis.com/operation-id"],
       template[0].containers,
-      template[0].labels["commit-sha"],
-      template[0].labels["goog-terraform-provisioned"],
-      template[0].labels["managed-by"],
+      template[0].labels
     ]
   }
 }
