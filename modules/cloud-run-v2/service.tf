967642c1367b388b49c148ccc8561e00281332a5
Maciej Sikora <maciej.sikora@rtbhouse.com>
2025-06-16 14:58:31 +0200

    docs && fmt

--- modules/cloud-run-v2/service.tf
+++ modules/cloud-run-v2/service.tf
@@ -529,8 +529,8 @@ locals {
 
   iap_member_list = toset(coalesce(var.iap_config.iam_additive, []))
 
-  use_iap_iam_binding = var.iap_config != null && var.iap_config.iam != null 
-  iap_binding_dict = local.use_iap_iam_binding ? { "iap" = var.iap_config.iam } : {}
+  use_iap_iam_binding = var.iap_config != null && var.iap_config.iam != null
+  iap_binding_dict    = local.use_iap_iam_binding ? { "iap" = var.iap_config.iam } : {}
 
 }
 
