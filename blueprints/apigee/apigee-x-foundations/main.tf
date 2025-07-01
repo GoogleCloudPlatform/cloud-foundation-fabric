a6504faa73ea50c2d2f923d48d18156187b672d5
Liam Nesteroff <lnesteroff@google.com>
2025-07-01 10:15:03 +1000

    modified apigee blueprint to use vpc_reuse with network_id passthrough

--- blueprints/apigee/apigee-x-foundations/main.tf
+++ blueprints/apigee/apigee-x-foundations/main.tf
@@ -63,9 +63,19 @@ module "shared_vpc" {
   source     = "../../../modules/net-vpc"
   project_id = var.project_config.shared_vpc_service_config.host_project
   name       = var.network_config.shared_vpc.name
-  vpc_reuse = {
-    use_data_source = true
-  }
+  vpc_reuse = (
+    var.network_config.shared_vpc.network_id == null
+    ? {
+      use_data_source = true
+    }
+    : {
+      user_data_source = false
+      attributes = {
+        network_id = var.network_config.shared_vpc.network_id
+      }
+    }
+  )
+
 }
 
 module "apigee_vpc" {
@@ -73,12 +83,7 @@ module "apigee_vpc" {
   source     = "../../../modules/net-vpc"
   project_id = module.project.project_id
   name       = coalesce(var.network_config.apigee_vpc.name, "apigee-vpc")
-  vpc_reuse = (
-    var.network_config.apigee_vpc.auto_create == true
-    ? {
-      use_data_source = true
-    } : null
-  )
+  vpc_reuse  = var.network_config.apigee_vpc.vpc_reuse
   psa_configs = [{
     ranges = merge(flatten([for k, v in var.apigee_config.instances : merge(
       v.runtime_ip_cidr_range == null ? {} : { "apigee-22-${k}" = v.runtime_ip_cidr_range },
