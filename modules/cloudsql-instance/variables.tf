efcceb8d7087e4a367fb0278a75ad5074b760a7d
Julio Castillo <jccb@google.com>
2025-07-17 09:44:28 +0200

    Remove validation already done by the provider.

--- modules/cloudsql-instance/variables.tf
+++ modules/cloudsql-instance/variables.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -247,14 +247,6 @@ variable "replicas" {
   }))
   default  = {}
   nullable = false
-  validation {
-    condition = alltrue([
-      for k, v in var.replicas :
-      contains(["ZONAL", "REGIONAL"], v.availability_type) || v.availability_type == null
-      ]
-    )
-    error_message = "Valid values for availability_type are 'ZONAL' or 'REGIONAL'."
-  }
 }
 
 variable "root_password" {
