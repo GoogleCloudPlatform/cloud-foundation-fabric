476ca1369ef2415fb580321700933ddaff7cbcae
Ludovico Magnocavallo <ludomagno@google.com>
2025-07-30 18:44:11 +0000

    output providers

--- fast-lite/bootstrap/output-files.tf
+++ fast-lite/bootstrap/output-files.tf
@@ -15,5 +15,33 @@
  */
 
 locals {
+  of_buckets = {
+    for k, v in module.factory.storage_buckets :
+    "$storage_buckets:${k}" => v
+  }
+  of_service_accounts = {
+    for k, v in module.factory.service_accounts :
+    "$iam_principals:service_accounts/${k}" => v
+  }
+  of_path = (
+    local.output_files.local_path == null
+    ? null
+    : pathexpand(local.output_files.local_path)
+  )
+  of_template = file("assets/providers.tf")
 }
-output "foo" { value = local.output_files }
+
+
+resource "local_file" "providers" {
+  for_each        = local.of_path == null ? {} : local.output_files.providers
+  file_permission = "0644"
+  filename        = "${local.of_path}/providers/${each.key}-providers.tf"
+  content = templatestring(local.of_template, {
+    bucket = lookup(
+      local.of_buckets, each.value.bucket, each.value.bucket
+    )
+    service_account = lookup(
+      local.of_service_accounts, each.value.service_account, each.value.service_account
+    )
+  })
+}
