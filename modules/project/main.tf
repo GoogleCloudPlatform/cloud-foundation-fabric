d1fbed59d234afc2f4c1af7db19945f63a256ba8
Ludovico Magnocavallo <ludomagno@google.com>
2025-10-04 11:57:27 +0200

    allow project id with universe prefix in project module (#3376)

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -68,7 +68,11 @@ locals {
       }
     )
   )
-  project_id         = "${local.universe_prefix}${local.prefix}${var.name}"
+  project_id = (
+    strcontains("${local.prefix}${var.name}", ":")
+    ? "${local.prefix}${var.name}"
+    : "${local.universe_prefix}${local.prefix}${var.name}"
+  )
   universe_prefix    = var.universe == null ? "" : "${var.universe.prefix}:"
   available_services = tolist(setsubtract(var.services, try(var.universe.unavailable_services, [])))
 }
