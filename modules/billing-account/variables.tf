a6371a8cb8529686e3b0dd9ec002b3f05997560c
Ludovico Magnocavallo <ludomagno@google.com>
2025-11-27 17:14:00 +0000

    re-enable project billing association in project factory, extends to folder

--- modules/billing-account/variables.tf
+++ modules/billing-account/variables.tf
@@ -79,8 +79,8 @@ variable "budgets" {
           }))
         }))
       }))
-      projects           = optional(list(string))
-      resource_ancestors = optional(list(string))
+      projects           = optional(list(string), [])
+      resource_ancestors = optional(list(string), [])
       services           = optional(list(string))
       subaccounts        = optional(list(string))
     }))
@@ -124,9 +124,11 @@ variable "context" {
   type = object({
     custom_roles          = optional(map(string), {})
     folder_ids            = optional(map(string), {})
+    folder_sets           = optional(map(list(string)), {})
     iam_principals        = optional(map(string), {})
     notification_channels = optional(map(string), {})
     project_ids           = optional(map(string), {})
+    project_sets          = optional(map(list(string)), {})
     storage_buckets       = optional(map(string), {})
   })
   default  = {}
