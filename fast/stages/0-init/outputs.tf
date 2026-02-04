7e7f5f092887458bd4c1c39a2c3c6f286175da9e
apichick <19226769+apichick@users.noreply.github.com>
2026-02-04 20:04:02 +0100

    Fast 0-init stage

--- fast/stages/0-init/outputs.tf
+++ fast/stages/0-init/outputs.tf
@@ -1,5 +1,16 @@
-output "org_policies_to_import_command" {
-  description = "Command to add org policies to import to terraform.tfvars in 0-org-setup stage."
+output "existing_org_policies" {
+  description = "Variable containing existing organization policies to be added to the terraform.tfvars of the 0-org-setup stage."
+  value       = <<-EOF
+    x = [
+    %{for val in local.active_org_policies~}
+      "${val}",
+    %{endfor~}
+    ]
+  EOF
+}
+
+output "project_id" {
+  description = "ID of the project to be used as billing project."
   value       = <<-EOF
     x = [
     %{for val in local.active_org_policies~}
