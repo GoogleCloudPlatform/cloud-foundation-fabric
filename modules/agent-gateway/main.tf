354e4bf7917363a6d3e495c523920e183e17ccd6
lucaprete <preteluca@gmail.com>
2026-04-10 18:15:15 +0200

    README and tests

--- modules/agent-gateway/main.tf
+++ modules/agent-gateway/main.tf
@@ -14,10 +14,30 @@
  * limitations under the License.
  */
 
+locals {
+  _ctx_p = "$"
+  ctx = {
+    for k, v in var.context : k => {
+      for kk, vv in v : "${local._ctx_p}${k}:${kk}" => vv
+    } if k != "condition_vars"
+  }
+  location = lookup(
+    local.ctx.locations, var.region, var.region
+  )
+  network_attachment_id = lookup(
+    local.ctx.psc_network_attachments,
+    var.networking_config.psc_i_network_attachment_id,
+    var.networking_config.psc_i_network_attachment_id
+  )
+  project_id = lookup(
+    local.ctx.project_ids, var.project_id, var.project_id
+  )
+}
+
 resource "google_network_services_agent_gateway" "default" {
   provider    = google-beta
-  project     = var.project_id
-  location    = var.region
+  project     = local.project_id
+  location    = local.location
   name        = var.name
   description = var.description
   labels      = var.labels
@@ -39,13 +59,13 @@ resource "google_network_services_agent_gateway" "default" {
 
   dynamic "network_config" {
     for_each = (
-      try(var.networking_config.psc_i_network_attachment_id, null) == null
+      try(local.network_attachment_id, null) == null
       ? [] : [""]
     )
 
     content {
       egress {
-        network_attachment = var.networking_config.psc_i_network_attachment_id
+        network_attachment = local.network_attachment_id
       }
     }
   }
