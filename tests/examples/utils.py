99f53d37f003ef534bea8127c0fbffd5b4a8e8a2
Julio Castillo <jccb@google.com>
2026-04-27 12:00:54 +0200

    New generate_plan_summary.py tool

--- tests/examples/utils.py
+++ tests/examples/utils.py
@@ -13,7 +13,10 @@
 # limitations under the License.
 
 import collections
+import os
 import re
+from pathlib import Path
+import marko
 
 Directive = collections.namedtuple('Directive', 'name args kwargs')
 TerraformExample = collections.namedtuple(
@@ -38,3 +41,71 @@ def get_tftest_directive(s):
         args.append(arg)
     return Directive(name, args, kwargs)
   return None
+
+
+def get_readme_examples(readme_path, fabric_root):
+  """Find all code examples tagged for testing in a README file."""
+  readme_path = readme_path.resolve()
+  fabric_root = fabric_root.resolve()
+  doc = marko.parse(readme_path.read_text())
+  module = readme_path.parent
+  path = module.relative_to(fabric_root)
+
+  files = collections.defaultdict(dict)
+  fixtures = {}
+  examples = []
+
+  # first pass: collect all examples tagged with tftest-file or tftest-fixture
+  last_header = None
+  for child in doc.children:
+    if isinstance(child, marko.block.FencedCode):
+      code = child.children[0].children
+      directive = get_tftest_directive(code)
+      if directive is None:
+        continue
+      if directive.name == 'tftest-file':
+        name, filepath = directive.kwargs['id'], directive.kwargs['path']
+        files[last_header][name] = File(filepath, code)
+      if directive.name == 'tftest-fixture':
+        name = directive.kwargs['id']
+        fixtures[name] = code
+    elif isinstance(child, marko.block.Heading):
+      last_header = child.children[0].children
+
+  # second pass: collect all examples tagged with tftest
+  last_header = None
+  index = 0
+  for child in doc.children:
+    if isinstance(child, marko.block.FencedCode):
+      index += 1
+      code = child.children[0].children
+      directive = get_tftest_directive(code)
+      if directive is None:
+        continue
+
+      if child.lang in ('hcl', 'tfvars'):
+        name = f'{path}:{last_header}'
+        if index > 1:
+          name += f' {index}'
+        example_id = f'terraform:{path}:{last_header}:{index}'
+
+        marks = []
+        if 'serial' in directive.args:
+          marks.append('serial')
+
+        example = TerraformExample(name, code, path, files[last_header],
+                                   fixtures, child.lang, directive)
+        examples.append((example, example_id, marks, last_header, index))
+      elif child.lang == "yaml":
+        schema = directive.kwargs.get('schema')
+        if directive.name == "tftest-file" and schema:
+          schema = module / 'schemas' / schema
+          example = YamlExample(code, module, schema)
+          yaml_path = directive.kwargs['path']
+          example_id = f'yaml:{path}:{last_header}:{yaml_path}:{index}'
+          examples.append((example, example_id, [], last_header, index))
+    elif isinstance(child, marko.block.Heading):
+      last_header = child.children[0].children
+      index = 0
+
+  return examples
