e02cc2d896283800b20183c6e24b226bcd18f90f
Simon Roberts <lyricnz@users.noreply.github.com>
2026-05-05 18:40:02 +1000

    First-pass migration of resources + tests

--- modules/net-vpc-firewall/outputs.tf
+++ modules/net-vpc-firewall/outputs.tf
@@ -17,14 +17,14 @@
 output "default_rules" {
   description = "Default rule resources."
   value = {
-    admin = try(google_compute_firewall.allow-admins, null)
-    http  = try(google_compute_firewall.allow-tag-http, null)
-    https = try(google_compute_firewall.allow-tag-https, null)
-    ssh   = try(google_compute_firewall.allow-tag-ssh, null)
+    admin = try(google_compute_firewall.allow_admins, null)
+    http  = try(google_compute_firewall.allow_tag_http, null)
+    https = try(google_compute_firewall.allow_tag_https, null)
+    ssh   = try(google_compute_firewall.allow_tag_ssh, null)
   }
 }
 
 output "rules" {
   description = "Custom rule resources."
-  value       = google_compute_firewall.custom-rules
+  value       = google_compute_firewall.custom_rules
 }
