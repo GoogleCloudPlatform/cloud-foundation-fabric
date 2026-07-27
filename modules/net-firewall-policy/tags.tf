0a44fb7d325ba099af30ff01729efb05bc50e4bc
Simone Ruffilli <sruffilli@google.com>
2026-07-27 16:37:34 +0200

    Fix tag binding parent path using numeric ID

--- modules/net-firewall-policy/tags.tf
+++ modules/net-firewall-policy/tags.tf
@@ -19,15 +19,20 @@ locals {
     for k, v in var.tag_bindings : k => lookup(local.ctx.tag_values, v, v)
   }
   location = local.use_hierarchical ? null : lookup(local.ctx.locations, var.region, var.region)
-  policy_id = one(concat(
-    google_compute_region_network_firewall_policy.net_regional[*].id,
-    google_compute_network_firewall_policy.net_global[*].id
+  _policy_numeric_id = one(concat(
+    google_compute_region_network_firewall_policy.net_regional[*].region_network_firewall_policy_id,
+    google_compute_network_firewall_policy.net_global[*].network_firewall_policy_id
   ))
+  policy_parent = local.use_hierarchical ? null : (
+    var.region == "global"
+    ? "//compute.googleapis.com/projects/${var.parent_id}/global/firewallPolicies/${local._policy_numeric_id}"
+    : "//compute.googleapis.com/projects/${var.parent_id}/regions/${var.region}/firewallPolicies/${local._policy_numeric_id}"
+  )
 }
 
 resource "google_tags_location_tag_binding" "binding" {
   for_each  = !local.use_hierarchical ? var.tag_bindings : {}
-  parent    = "//compute.googleapis.com/${local.policy_id}"
+  parent    = local.policy_parent
   location  = local.location
   tag_value = templatestring(local._tag_bindings[each.key], var.context.tag_vars)
 }
