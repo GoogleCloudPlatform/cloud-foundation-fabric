d6e9eac137b5033b6ce27650440b75f005f0d01f
Suryansh Singhal <singhalsuryansh.0306@gmail.com>
2026-08-31 11:56:29 +0530

    removed deprecated attibute as per https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster#desired_tier-1:~:text=enterprise_config%20block%20supports%3A-,desired_tier,-%2D%20(Optional)%20(DEPRECATED)%20Sets
    backup plan id needs to be exported through output as the id needs to be refrenced via the restore plan

--- modules/gke-cluster-standard/outputs.tf
+++ modules/gke-cluster-standard/outputs.tf
@@ -14,6 +14,13 @@
  * limitations under the License.
  */
 
+output "backup_plan_id" {
+  description = "GKE backup plan ids."
+  value = {
+    for k, v in google_gke_backup_backup_plan.backup_plan : k => v.id
+  }
+}
+
 output "ca_certificate" {
   description = "Public certificate of the cluster (base64-encoded)."
   value = (
