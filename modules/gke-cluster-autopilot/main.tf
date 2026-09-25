63b1c40dd83bbd74f5454bb2dadfe50cdfe16621
marat-akhmetianov <m.akhmetianov@sxope.com>
2026-09-25 09:52:20 +0100

    Add anonymous authentication configuration to GKE modules

--- modules/gke-cluster-autopilot/main.tf
+++ modules/gke-cluster-autopilot/main.tf
@@ -60,6 +60,12 @@ resource "google_container_cluster" "cluster" {
       enabled = var.backup_configs.enable_backup_agent
     }
   }
+  dynamic "anonymous_authentication_config" {
+    for_each = var.anonymous_authentication_config != null ? [""] : []
+    content {
+      mode = var.anonymous_authentication_config.mode
+    }
+  }
   dynamic "authenticator_groups_config" {
     for_each = var.enable_features.groups_for_rbac != null ? [""] : []
     content {
